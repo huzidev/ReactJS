ac8a78f233b771bb07973f8838a23ddedc03b3c3
huzidev <huzaifa.iqdev@gmail.com>
2022-03-21 20:40:22 +0500

    JSX

--- React/todo-list/src/App.js
+++ React/todo-list/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    // that's all JSX NOT HTML it's javascript syntax extension
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
