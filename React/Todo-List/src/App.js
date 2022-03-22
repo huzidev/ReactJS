c3226d0ab59fc25379878881bf899d1ab1b86728
huzidev <huzaifa.iqdev@gmail.com>
2022-03-22 15:53:37 +0500

    JSX

--- React/Todo-List/src/App.js
+++ React/Todo-List/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     // that's all JSX NOT HTML it's javascript syntax extension
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    // we've to wrap all the html written throught JSX in something if there is nothing then simply wrap it in empty as we did their
+    <> 
+      <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Todos List</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -18,20 +19,6 @@ function App() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
             <form className="d-flex">
---
+++ React/Todo-List/src/MyComponents/header.js
@@ -0,0 +1,10 @@
+// there we'll write JSX for header file then simply we'll link it to the main app.js by IMPORTING
+
+// we can use rfc called ReactFunctionalComponent
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>header</div>
+  )
+}
