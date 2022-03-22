30a1b99fda646cec312aeda85aa133fce8b2182a
huzidev <huzaifa.iqdev@gmail.com>
2022-03-22 16:17:02 +0500

    PROPS

--- React/Todo-List/src/MyComponents/header.js
+++ React/Todo-List/src/MyComponents/header.js
@@ -4,11 +4,16 @@
 
 import React from 'react'
 
-export default function header() { // remeber THERE we've used export default but when we uses export const then in the main (APP.JS) we've to use {} then use the name of the file where JSX is written
+
+// props is JS object which takes data from parent to child and we've to use PROPS wherever we wanted to use infomation or data or arguments form parent to child as their
+
+// and we simply uses { props.tittle } as we did their and we already assigned tittle = My Todo List in main parent functions
+
+export default function header(props) { // remeber THERE we've used export default but when we uses export const then in the main (APP.JS) we've to use {} then use the name of the file where JSX is written
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Todos List</a>
+          <a className="navbar-brand" href="#">{props.tittle}</a> 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
