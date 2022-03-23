88bd1ed060eb8c0604bb9397b432d88f55f1bf63
huzidev <huzaifa.iqdev@gmail.com>
2022-03-23 19:57:26 +0500

    nav-bar for REACT

--- React/React-JS/Nav-Bar/script/JS.js
+++ React/React-JS/Nav-Bar/script/JS.js
@@ -1,16 +1,9 @@
-// render means start or to print on to the screen we use this documentgebID because this is used so we can tell ours code that where we wanted to add h1 and here root is like ours container for complete screen
-
-// REACTDOM is a global variable
-
-// will render (print, play) h1 inside div called (ROOT)
-
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"))
-
-// remeber we select documnet.gebID so we can tell that where we wanted to add ours paragraph
-
-ReactDOM.render(<p>Hello, This Is ReactJS</p>, document.getElementById("para"))
-
 ReactDOM.render(
-    <ul><li>hello</li><li>asdasdsadas</li></ul>, 
-    document.getElementById("list")
+
+    <div>
+
+        <Navbar />
+
+    </div>
+
 )
