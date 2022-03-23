7bf1a647953320b0d21954978041a0ab15705389
huzidev <huzaifa.iqdev@gmail.com>
2022-03-23 19:12:02 +0500

    comment

--- React/React-JS/script/JS.js
+++ React/React-JS/script/JS.js
@@ -1,1 +1,2 @@
+// render means start or to print on to the screen we use this documentgebID because this is used so we can tell ours code that where we wanted to add h1 and here root is like ours container for complete screen
 ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"))
