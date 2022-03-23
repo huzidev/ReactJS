0cd7b195e06792e79fcfea21cdaf7f5ebba0d8e2
huzidev <huzaifa.iqdev@gmail.com>
2022-03-23 19:13:25 +0500

    React DOm

--- React/React-JS/script/JS.js
+++ React/React-JS/script/JS.js
@@ -1,2 +1,6 @@
 // render means start or to print on to the screen we use this documentgebID because this is used so we can tell ours code that where we wanted to add h1 and here root is like ours container for complete screen
+
+// REACTDOM is a global variable
+
+// will render (print) h1 inside div called (ROOT)
 ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"))
