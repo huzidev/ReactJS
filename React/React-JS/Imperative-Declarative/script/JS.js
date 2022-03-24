532ec75f4d397700106c70f1c4a2bbb52b7a1ae3
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 14:13:07 +0500

    formated doc

--- React/React-JS/Imperative-Declarative/script/JS.js
+++ React/React-JS/Imperative-Declarative/script/JS.js
@@ -1,9 +1,9 @@
 // imperative is the way in which we've to specify every single thing like below
+
 const h1 = document.createElement("h1")
 h1.textContent = "Hello, This Is Imperative Way!"
 h1.className = "header"
 document.getElementById("root").append(h1) // apend meana add and we added h1 to div with id ROOT
-
 
 // declarative is the way in which computer will know what to do and how to do and REACT is declarative
 
