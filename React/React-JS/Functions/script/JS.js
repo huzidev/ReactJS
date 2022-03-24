935e0c56cccd3d88f83a64044ba664d94d04165a
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 16:25:08 +0500

    DOM

--- React/React-JS/Functions/script/JS.js
+++ React/React-JS/Functions/script/JS.js
@@ -4,7 +4,23 @@ function Fact() {
     // our function will RETURN whatever is written inside of it whenever the function is called
     return(
 
-
+        <div>
+            <h1>Reasons Why I Like To Code In React</h1>
+            <ol>
+                <li>It is really interesting</li>
+                <li>It is highly demandable</li>
+                <li>It has functions similar to JS, HTML</li>
+                <li>It is really easy to learn</li>
+                <li>It can help me to craete Single-Page Applications!</li>
+            </ol>
+        </div>
 
     )
 }
+
+ReactDOM.render(
+    
+    <Fact />,
+    document.getElementById("head")
+
+)
