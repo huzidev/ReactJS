0953629753c1efe64188297b2415b5a64327d1e6
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 16:09:44 +0500

    erros fixed

--- React/React-JS/Static-WebPage/script/JS.js
+++ React/React-JS/Static-WebPage/script/JS.js
@@ -3,7 +3,7 @@ const page = (
     <div>
         <img src="../assets/react.png" alt="React-Img" />
         <h1>My React Web-Page</h1>
-        <h3>Why i like react?</h3>
+        <h3>Why I like react?</h3>
         <ol>
             <li>It's Composable</li>
             <li>It's Declarative</li>
