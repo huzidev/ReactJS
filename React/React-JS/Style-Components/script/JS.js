18b608b36d6c5321ffab235e1dc29376a5df7165
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 19:19:30 +0500

    Logo-added

--- React/React-JS/Style-Components/script/JS.js
+++ React/React-JS/Style-Components/script/JS.js
@@ -1,8 +1,25 @@
+function Nav() {
+
+    return(
+        <nav>
+            <img src="../assets/react.png" alt="" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+            </ul>
+        </nav>
+    )
+
+}
+
+
 function Main() {
     return(
-
-        <h1>Hello</h1>
-
+        <div>
+            <Nav />
+            <h1>Hello</h1>
+        </div>
     )
 }
 
