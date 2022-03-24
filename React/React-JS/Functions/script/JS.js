ec6e9712459d6abba94e784563923eb972cf612b
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 16:21:15 +0500

    functions react

---
+++ React/React-JS/Functions/script/JS.js
@@ -0,0 +1,10 @@
+//REMEMBER while creating fucntions make sure to write the first word Capital of that fucntion
+
+function Fact() {
+    // our function will RETURN whatever is written inside of it whenever the function is called
+    return(
+
+
+
+    )
+}
