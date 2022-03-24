07355a9bad835efbb3054f8ab50637f00b1480cf
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 15:27:58 +0500

    wrapping elements

--- React/React-JS/JSX/script/JS.js
+++ React/React-JS/JSX/script/JS.js
@@ -1,5 +1,16 @@
 //JSX (javascript syntax extension OR javascript XML)
+// MAKE SURE in JSX we've to return only one parent OR wrap all the other elements in the single parent as we did below
+
 
 const element = <h1 className="header">Hello, This Is JSX(Javascript XML)</h1>
 
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(element, document.getElementById("head"))
+
+ReactDOM.render(
+    <div>
+        <h1>Hello</h1>
+        <p>Hello</p>
+    </div>, 
+    document.getElementById("root")
+)
+
