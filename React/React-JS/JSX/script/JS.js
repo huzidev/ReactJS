6f51d31b38243985e98cc5e728508f58dc1b01d5
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 15:21:42 +0500

    comment

--- React/React-JS/JSX/script/JS.js
+++ React/React-JS/JSX/script/JS.js
@@ -1,1 +1,5 @@
+//JSX (javascript syntax extension OR javascript XML)
+
 const element = <h1 className="header">Hello, This Is JSX(Javascript XML)</h1>
+
+ReactDOM.render(element, document.getElementById("root"))
