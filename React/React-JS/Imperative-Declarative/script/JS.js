816d3cf40cc956d88b5ab560a1830c7052c48d31
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 09:07:02 +0500

    react dim

---
+++ React/React-JS/Imperative-Declarative/script/JS.js
@@ -0,0 +1,6 @@
+// IMPERATIVE way is describe below in which we've to tell or specify every single thing
+
+const h1 = document.createElement("h1")
+h1.textContent = "This Is Imperative Way Of Writing React"
+h1.className = "header"
+document.getElementById("root").append(h1)
