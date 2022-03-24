d2b340e5fd69f2a2756e9ccbc0d248fbbec02a00
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 21:21:43 +0500

    Organised Components

---
+++ React/React-JS/Organised-Components/script/MyComponents/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+
+    return(
+        <nav>
+            <img src="../assets/react.png" alt="React-Logo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+            </ul>
+        </nav>
+    )
+
+}
