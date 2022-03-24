dce225f61620ae9fc640750944d5264940c8288e
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 15:40:35 +0500

    Nav-Bar

---
+++ React/React-JS/Nav-JSX/script/JS.js
@@ -0,0 +1,16 @@
+const nav = (
+
+    <div>
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(
+    nav,
+    document.getElementById("head")
+
+)
