ddb9eed14833adec077c538c0466224a4dd4c302
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 16:26:22 +0500

    return function

--- React/React-JS/Functions/script/JS.js
+++ React/React-JS/Functions/script/JS.js
@@ -1,7 +1,7 @@
 //REMEMBER while creating fucntions make sure to write the first word Capital of that fucntion
 
 function Fact() {
-    // our function will RETURN whatever is written inside of it whenever the function is called
+    // our function will (RETURN) whatever is written inside of it whenever the function is called
     return(
 
         <div>
