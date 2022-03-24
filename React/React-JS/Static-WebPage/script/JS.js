08ea9f64f323834ef96290b26751ccd7646ea0f7
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 15:57:42 +0500

    render DOM

---
+++ React/React-JS/Static-WebPage/script/JS.js
@@ -0,0 +1,15 @@
+const page = (
+
+    <div>
+        <h1>My React Web-Page</h1>
+        <h3>Why i like react?</h3>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ol>
+    </div>
+
+)
