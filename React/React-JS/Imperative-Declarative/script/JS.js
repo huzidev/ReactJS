4bd14b9f005608b9e1423121b3b0e2bb7568cbd3
huzidev <huzaifa.iqdev@gmail.com>
2022-03-24 14:11:02 +0500

    imperative declarative comp

--- React/React-JS/Imperative-Declarative/script/JS.js
+++ React/React-JS/Imperative-Declarative/script/JS.js
@@ -1,4 +1,8 @@
-// import react from 'react'
-// import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"))
+const h1 = document.createElement("h1")
+h1.textContent = "Hello, This Is Imperative Way!"
+h1.className = "header"
+document.getElementById("root").append(h1)
+
+
+ReactDOM.render(<h1>Hello, This Is Declarative Way!</h1>, document.getElementById("head"))
