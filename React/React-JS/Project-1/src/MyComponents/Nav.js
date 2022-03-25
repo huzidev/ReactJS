44c0116c22d679247b85f123f9ff59a34602d76e
huzidev <huzaifa.iqdev@gmail.com>
2022-03-25 18:54:54 +0500

    fixing nav

--- React/React-JS/Project-1/src/MyComponents/Nav.js
+++ React/React-JS/Project-1/src/MyComponents/Nav.js
@@ -5,7 +5,8 @@ import Logo from './MyComponents/react.png';
 export default function Nav() {
   return (
     <nav>
-
+        <img src="{Logo}" alt="React-Logo" />
+        <h3 className="react">ReactJS</h3>
     </nav>
   )
 }
