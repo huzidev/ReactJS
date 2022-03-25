e1fcc1819bf8df9af349a2a4bba1c600e940f41d
huzidev <huzaifa.iqdev@gmail.com>
2022-03-25 19:22:07 +0500

    ul

---
+++ React/React-JS/Project-1/src/MyComponents/Ul.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Ul() {
+  return (
+      <div>
+          <ul>
+              <li>ReactJS Library of JS</li>
+              <li>Easy to learn</li>
+              <li>Easy to code</li>
+              <li>Highly Advance</li>
+              <li>Similar to JS, HTML</li>
+          </ul>
+      </div>
+  )
+}
