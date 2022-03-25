ed5ff7c5183144b0eebbaa466880bbef8c9d1fe9
huzidev <huzaifa.iqdev@gmail.com>
2022-03-25 11:09:03 +0500

    logo fix

---
+++ React/React-JS/Organised-Components/src/MyComponents/Nav.js
@@ -0,0 +1,22 @@
+// Export so we can export it from their and import it at main (PARENT) file
+
+import React from 'react';
+import logo from '../assets/react.png';
+
+// REMEMBER while importing images make sure that the assets folder should've to be in the same folder as of where main PARENT index.js file is present
+// here we just make a refrence that import (react.png) and assigned it the name logo then simply insert the same name in src={}
+
+export default function Nav() {
+
+    return(
+        <nav>
+            <img src={logo} alt="React-Logo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+            </ul>
+        </nav>
+    )
+
+}
