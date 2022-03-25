fa394ae3194e48eb4ce8586f14111fb2a350ef84
huzidev <huzaifa.iqdev@gmail.com>
2022-03-25 18:35:18 +0500

    React Project

---
+++ React/React-JS/Project-1/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './MyComponents/Nav';
+import Header from './MyComponents/Header';
+import Ul from './MyComponents/Ul';
+import Footer from './MyComponents/Footer';
+import './styles/style.css';
+// it is better to use import css here rather to use linked css file in HTML
+
+function Main() {
+    return(
+        <div>
+            <Nav />
+            <Header />
+            <Ul />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(
+
+    <Main />,
+    document.getElementById("head")
+
+)
