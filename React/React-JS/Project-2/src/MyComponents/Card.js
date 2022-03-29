c7d5cf4819aa0d67806a974b48cb9b520eb1fa2c
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 18:25:54 +0500

    project-2

--- React/React-JS/Project-2/src/MyComponents/Card.js
+++ React/React-JS/Project-2/src/MyComponents/Card.js
@@ -5,52 +5,12 @@ import Slide2 from '../assets/slider3.jpg';
 
 export default function Card() {
   return (
-      <div className="card">
-        <img src={Slide} className="card-img" alt="Slide-1" />
-        <div className="card-1">
-            <span className="color">
-                5.0
-            </span>
-            <span className="color">
-                Japan
-            </span>
+    <>
+        <div className="card">
+            <img src={Slide} className="card-img" alt="Slide-1" />
+            <img src={Slide1} className="card-img" alt="Slide-2" />
+            <img src={Slide2} className="card-img" alt="Slide-3" />
         </div>
-        <p>
-            Kakarot
-        </p>
-        <p>
-            Latest Update - <span className="bold"> Ultra Instinct </span>
-        </p>
-        <img src={Slide1} className="card-img" alt="Slide-2" />
-        <div className="card-1">
-            <span className="color">
-                5.0
-            </span>
-            <span className="color">
-                Japan
-            </span>
-        </div>
-        <p>
-            Vegeta
-        </p>
-        <p>
-            Latest Update - <span className="bold"> Super-Saiyan-Blue </span>
-        </p>
-        <img src={Slide2} className="card-img" alt="Slide-3" />
-        <div className="card-1">
-            <span className="color">
-                5.0
-            </span>
-            <span className="color">
-                Japan
-            </span>
-        </div>
-        <p>
-            Vegeta
-        </p>
-        <p>
-            Latest Update - <span className="bold"> Super-Saiyan-Blue </span>
-        </p>
-      </div>
+     </>
   )
 }
