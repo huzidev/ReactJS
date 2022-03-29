027255e108cb51a51c79d7d96dbb5f9ccb285594
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:02:05 +0500

    paragrapgh

--- React/React-JS/Project-2/src/MyComponents/Header.js
+++ React/React-JS/Project-2/src/MyComponents/Header.js
@@ -5,8 +5,11 @@ export default function Header() {\
 
   return (
       <section>
-          <img src={main} alt="header-img" />
+          <img src={main} className="main-img" alt="header-img" />
           <h1 className="main-header">React JS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio assumenda repellat error ipsam, unde, aperiam dolorum quasi, accusamus fugit laudantium consectetur harum tenetur fugiat amet. Molestiae vero voluptate eos aspernatur.
+          </p>
       </section>
   )
 
