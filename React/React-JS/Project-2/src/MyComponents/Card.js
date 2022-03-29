27f3a1bcb11c4912063ac4d6c2f7c1df5bf6e320
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 17:48:28 +0500

    card class

--- React/React-JS/Project-2/src/MyComponents/Card.js
+++ React/React-JS/Project-2/src/MyComponents/Card.js
@@ -4,7 +4,7 @@ import Slide from '../assets/slider.jpg';
 export default function Card() {
   return (
       <div className="card">
-        <img src={Slide} alt="Slide-1" />
+        <img src={Slide} className="card-img" alt="Slide-1" />
         <div className="card-1">
             <span>
                 5.0
