0cc1f9b6a42b8238f745bb0f3910b30125bc54a9
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 09:50:03 +0500

    indentations

--- React/React-JS/Project-2/src/MyComponents/Nav.js
+++ React/React-JS/Project-2/src/MyComponents/Nav.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Logo from '../assets/air-bnb.png';
 
 export default function Nav() {
+
   return (
     <nav>
       <img src={Logo} className="nav-logo" alt="Air-Bnb-Logo" />
     </nav>
   )
+  
 }
