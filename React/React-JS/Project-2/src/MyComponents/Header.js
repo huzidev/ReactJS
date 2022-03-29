7b129e3f48ac00ef780ad5ec0382caee793f8d30
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:00:29 +0500

    section

---
+++ React/React-JS/Project-2/src/MyComponents/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import main from '../assets/main.jpg'
+export default function Header() {
+  return (
+      <section>
+          <img src={main} alt="header-img" />
+      </section>
+  )
+}
