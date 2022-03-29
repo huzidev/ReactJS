1fced00ddb39059f11e0ad7beabbad77ead6d06b
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:25:21 +0500

    formated documnets

--- React/React-JS/Project-2/src/MyComponents/Card.js
+++ React/React-JS/Project-2/src/MyComponents/Card.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Slide from '../assets/slider';
 
 export default function Card() {
   return (
       <div className="card">
-          
+          <img src={Slide} alt="Slide-1" />
       </div>
   )
 }
