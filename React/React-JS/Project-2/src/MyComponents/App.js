31f16419bb8cac95c57303297203cd835a748153
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:04:14 +0500

    render components

--- React/React-JS/Project-2/src/MyComponents/App.js
+++ React/React-JS/Project-2/src/MyComponents/App.js
@@ -1,9 +1,11 @@
 import Nav from '../MyComponents/Nav';
+import Header from '../MyComponents/Header';
 
 export default function App() {
     return(
         <div>
             <Nav />
+            <Header />
         </div>
     )
 }
