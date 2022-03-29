63ebdfba346151c0aaef2c80ca4979dfb6bed6d5
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:32:06 +0500

    render functions

--- React/React-JS/Project-2/src/MyComponents/App.js
+++ React/React-JS/Project-2/src/MyComponents/App.js
@@ -1,11 +1,14 @@
 import Nav from '../MyComponents/Nav';
 import Header from '../MyComponents/Header';
+import Header from '../MyComponents/Card';
+import Card from '../MyComponents/Card';
 
 export default function App() {
     return(
         <div>
             <Nav />
             <Header />
+            <Card />
         </div>
     )
 }
