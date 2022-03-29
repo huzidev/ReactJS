343e3998602ea7b30c9d3b93e80ae2948fc5902c
huzidev <huzaifa.iqdev@gmail.com>
2022-03-29 16:31:29 +0500

    card-slider

--- React/React-JS/Project-2/src/MyComponents/Card.js
+++ React/React-JS/Project-2/src/MyComponents/Card.js
@@ -4,10 +4,25 @@ import Slide from '../assets/slider';
 export default function Card() {
   return (
       <div className="card">
-          <img src={Slide} alt="Slide-1" />
-          <div className="card-1">
-          <i className="fa-solid fa-star"></i>
-          </div>
+        <img src={Slide} alt="Slide-1" />
+        <div className="card-1">
+            <i className="fa-solid fa-star"></i>
+            <span>
+                5.0
+            </span>
+            <span>
+                (6) .
+            </span>
+            <span>
+                Japan
+            </span>
+            <p>
+                Kakarot
+            </p>
+            <p>
+                Latest Update - Ultra Instinct
+            </p>
+        </div>
       </div>
   )
 }
