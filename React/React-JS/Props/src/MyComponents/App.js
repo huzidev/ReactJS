cc4b3c50da23a21cbe2a40b4e176e41beba4e658
huzidev <huzaifa.iqdev@gmail.com>
2022-03-30 11:18:14 +0500

    contact card created

--- React/React-JS/Props/src/MyComponents/App.js
+++ React/React-JS/Props/src/MyComponents/App.js
@@ -7,6 +7,8 @@ import Img3 from "../assets/slider3.jpg"
 
 export default function App() {
     // we are just simply making props here and we'll link it in the main file which is CONTACT.js there we'll pass (props) as parameter then we'll link all the attribute there with {props/img}, {props.name} etc
+    
+    // it is ok to not to separate them with commas
     return (
         <div className="contacts">
             <Contact 
