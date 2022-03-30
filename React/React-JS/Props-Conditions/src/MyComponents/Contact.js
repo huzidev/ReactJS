6313ddc486232a2fdffd34f46ee8ea680a187788
huzidev <huzaifa.iqdev@gmail.com>
2022-03-30 17:46:46 +0500

    contact info

--- React/React-JS/Props-Conditions/src/MyComponents/Contact.js
+++ React/React-JS/Props-Conditions/src/MyComponents/Contact.js
@@ -9,23 +9,13 @@ import React from "react"
 export default function Contact(props) {
    
     return (
-        <div className="contact-card">
+        <div>
+            <h3>
 
-            <img src={props.img} alt="Img"/>
-
-            <h3>{props.name}</h3>
-
-            <div className="info-group">
-
-                <p>{props.status}</p>
-
-            </div>
-
-            <div className="info-group">
-
-                <p>{props.info} <b>{props.bold}</b></p>
+            </h3>
+            <p>
                 
-            </div>
+            </p>
         </div>
     )
 
