7c0b74f00080b794da1c1be8fa79deaf25f3205f
huzidev <huzaifa.iqdev@gmail.com>
2022-03-30 10:54:22 +0500

    indentation fix

--- React/React-JS/Props/src/MyComponents/Contact.js
+++ React/React-JS/Props/src/MyComponents/Contact.js
@@ -7,7 +7,7 @@ export default function Contact(props) {
      */
     return (
         <div className="contact-card">
-            <img src={props.img}/>
+            <img src={props.img} alt="Img"/>
             <h3>{props.name}</h3>
             <div className="info-group">
                 <p>{props.phone}</p>
--- React/React-JS/Props/src/index.js
+++ React/React-JS/Props/src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./styles/style.css"
-
 import App from "./MyComponents/App"
+import "./styles/style.css"
 
 ReactDOM.render(<App />, document.getElementById("head"))
