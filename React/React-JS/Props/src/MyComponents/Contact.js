21b96ef854336d12caa338ba83786a2e2d32a39e
huzidev <huzaifa.iqdev@gmail.com>
2022-03-30 11:09:02 +0500

    pops

--- React/React-JS/Props/src/MyComponents/Contact.js
+++ React/React-JS/Props/src/MyComponents/Contact.js
@@ -1,18 +1,27 @@
 import React from "react"
 
+// it is not mandatory to write exact props (Property) rather we can use whatever name we like to make props BUT it is good practice to write props cause it is short form of (PROPERTY)
 export default function Contact(props) {
    
     return (
         <div className="contact-card">
+
             <img src={props.img} alt="Img"/>
+
             <h3>{props.name}</h3>
+
             <div className="info-group">
+
                 <p>{props.phone}</p>
+
             </div>
+
             <div className="info-group">
+
                 <p>{props.email}</p>
+
             </div>
         </div>
     )
-    
+
 }
