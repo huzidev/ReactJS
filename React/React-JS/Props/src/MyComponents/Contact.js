ce91a72e7883176e4926f5699c3f51d1f0621e72
huzidev <huzaifa.iqdev@gmail.com>
2022-03-30 11:20:17 +0500

    receiving props

--- React/React-JS/Props/src/MyComponents/Contact.js
+++ React/React-JS/Props/src/MyComponents/Contact.js
@@ -1,6 +1,11 @@
 import React from "react"
 
 // it is not mandatory to write exact props (Property) rather we can use whatever name we like to make props BUT it is good practice to write props cause it is short form of (PROPERTY)
+
+// by creating PROPS we can make ours components more and more reuseable as we did over there
+
+// and props basically receive a javascript object
+
 export default function Contact(props) {
    
     return (
