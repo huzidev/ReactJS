6a57bfd77cdf40ddc5d955a19feea80a8578044f
huzidev <huzaifa.iqdev@gmail.com>
2022-03-31 18:11:10 +0500

    API

--- React/React-JS/Mapping-Advance/src/MyComponents/data.js
+++ React/React-JS/Mapping-Advance/src/MyComponents/data.js
@@ -7,6 +7,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
     {
+        Id: 1,
         Img: "main.jpg",
         title: "Learning ReactJS Mapping",
         description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
@@ -19,6 +20,7 @@ export default [
         HoursSpend: 5
     },
     {
+        Id: 2,
         Img: "slider.jpg",
         title: "Learning ReactJS Mapping",
         description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
@@ -31,6 +33,7 @@ export default [
         HoursSpend: 5
     },
     {
+        Id: 3,
         Img: "slider3.jpg",
         title: "Learning ReactJS Mapping",
         description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
