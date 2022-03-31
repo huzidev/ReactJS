63a203b7a797351ecdcfd74a865f9a1d22b00ba1
huzidev <huzaifa.iqdev@gmail.com>
2022-03-31 18:24:27 +0500

    props better

--- React/React-JS/Mapping-Advance/src/MyComponents/Contact.js
+++ React/React-JS/Mapping-Advance/src/MyComponents/Contact.js
@@ -1,9 +1,9 @@
 import React from "react";
-// it is not mandatory to write exact props (Property) rather we can use whatever name we like to make props BUT it is good practice to write props cause it is short form of (PROPERTY)
+// it is not mandatory to write exact props.info (Property) rather we can use whatever name we like to make props.info BUT it is good practice to write props.info cause it is short form of (PROPERTY)
 
-// by creating PROPS we can make ours components more and more reuseable as we did over there
+// by creating props.info we can make ours components more and more reuseable as we did over there
 
-// and props basically receive a javascript object
+// and props.info basically receive a javascript object
 
 // NOTE sometime if images fail to load then simply move yours image folder to public folder where HTML file is present because BY DEFAULT we've access to public folder therefore we've move ours assets folder to public now it'll works
 
@@ -14,12 +14,12 @@ export default function Contact(props) {
     // it is better to use === instead of == because === check conditions more thoroughly
    let detail
 
-   if( props.power === "Max" ){ // means if data says power is max then print Maximum in the badge
+   if( props.info.Power === "Max" ){ // means if data says power is max then print Maximum in the badge
        detail = "Maximum"
 
    }
 
-   else if( props.power === "Not-Max" ){
+   else if( props.info.Power === "Not-Max" ){
 
        detail = "Not-Maximum"
 
@@ -30,15 +30,15 @@ export default function Contact(props) {
             <div className="banner">
                 {detail}
             </div>
-            <img src={`../assets/${props.img}`} alt="Img" className="card-img"/>
-            <h3>Tittle : {props.title}</h3>
+            <img src={`../assets/${props.info.Img}`} alt="Img" className="card-img"/>
+            <h3>Tittle : {props.info.title}</h3>
             <div>
-                <p> <b> Description : </b> {props.des}</p>
-                <p> <b> Experience : </b> {props.experience}</p>
-                <p> <b> Stats-Languages : </b> {props.language}+</p>
-                <p> <b> Stats-Fvrt-Languages : </b> {props.fvrt}</p>
-                <p> <b> Source : </b> {props.source}</p>
-                <p> <b> Hours : </b> {props.hours}+</p>
+                <p> <b> Description : </b> {props.info.description}</p>
+                <p> <b> Experience : </b> {props.info.experience}</p>
+                <p> <b> Stats-Languages : </b> {props.info.stats.Languages}+</p>
+                <p> <b> Stats-Fvrt-Languages : </b> {props.info.stats.Favorites}</p>
+                <p> <b> Source : </b> {props.info.Source}</p>
+                <p> <b> Hours : </b> {props.info.HoursSpend}+</p>
             </div>
         </div>
     )
