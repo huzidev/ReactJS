629c31e6beb62fec986eace1301c5116beb391b6
huzidev <huzaifa.iqdev@gmail.com>
2022-03-31 09:51:23 +0500

    map

--- React/React-JS/Mapping/src/MyComponents/App.js
+++ React/React-JS/Mapping/src/MyComponents/App.js
@@ -3,7 +3,16 @@ import Data from "./data";
 import Contact from "./Contact";
 
 export default function App() {
+    // we use map to make ours code more self-sustaining so when ever user insert any data from web-page it'll be store in Data component form where we've already link it there so it can update automatically called self-sustaining
+
+    // const cards is the name of variable so we can return complete function when rendering just by typing {cards}
+
+
+    // <Contact is the component where all of ours MAIN JSX is written where we'll link all the info with the help of props
     
+    // info.img, info.tittle is basically from Data part and info is like props we can name it whatever we want than return
+
+    // Data is the name of ours component where all the user data is present 
    const cards = Data.map(info => {
        return(
            <Contact
