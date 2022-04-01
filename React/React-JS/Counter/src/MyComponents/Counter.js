cb5e58339be9c27e66a0b2a168ec57fb34639214
huzidev <huzaifa.iqdev@gmail.com>
2022-04-01 19:26:16 +0500

    boiler plated

--- React/React-JS/Counter/src/MyComponents/Counter.js
+++ React/React-JS/Counter/src/MyComponents/Counter.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Counter() {
   return (
       <div className="Counter">
-
+          <button className="Minus">
+              -
+          </button>
+          <div className="Count">
+              <h1>
+                  0
+              </h1> 
+          </div>
+          <button className="Plus">
+              +
+          </button>
       </div>
   )
 }
