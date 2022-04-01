8f7e8a1bafc3dbcd653db3702705a70cf63cedc5
huzidev <huzaifa.iqdev@gmail.com>
2022-04-01 18:30:12 +0500

    border fix

--- React/React-JS/OnClick/src/MyComponents/Button.js
+++ React/React-JS/OnClick/src/MyComponents/Button.js
@@ -6,7 +6,7 @@ export default function Button(props)
 
   return (
 
-      <button onClick={props.click}>
+      <button onClick={props.click} className="Btn">
           Add-Item
       </button>
 
