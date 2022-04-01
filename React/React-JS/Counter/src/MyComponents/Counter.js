04f52d7837199ee22d7e21df6d6654fe96d031e6
huzidev <huzaifa.iqdev@gmail.com>
2022-04-01 19:58:40 +0500

    counter fix

--- React/React-JS/Counter/src/MyComponents/Counter.js
+++ React/React-JS/Counter/src/MyComponents/Counter.js
@@ -3,15 +3,15 @@ import React from 'react'
 export default function Counter(props) {
   return (
       <div className="Counter">
-          <button className="Minus" onClick={Add}>
+          <button className="Minus" onClick={props.sub}>
               -
           </button>
           <div className="Count">
               <h1>
-                  {props.Count}
+                  {props.count}
               </h1> 
           </div>
-          <button className="Plus">
+          <button className="Plus" onClick={props.add}>
               +
           </button>
       </div>
