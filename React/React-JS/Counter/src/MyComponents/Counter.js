485858e0205a471cf08a8f2645c65c8df4bb474f
huzidev <huzaifa.iqdev@gmail.com>
2022-04-01 21:36:13 +0500

    indentation

--- React/React-JS/Counter/src/MyComponents/Counter.js
+++ React/React-JS/Counter/src/MyComponents/Counter.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
 export default function Counter(props) {
-  return (
-      <div className="Counter">
-          <button className="Minus" onClick={props.sub}>
-              -
-          </button>
-          <div className="Count">
-              <h1>
-                  {props.count}
-              </h1> 
-          </div>
-          <button className="Plus" onClick={props.add}>
-              +
-          </button>
-      </div>
-  )
+
+    return (
+        <div className="Counter">
+            <button className="Minus" onClick={props.sub}>
+                -
+            </button>
+            <div className="Count">
+                <h1>
+                    {props.count}
+                </h1> 
+            </div>
+            <button className="Plus" onClick={props.add}>
+                +
+            </button>
+        </div>
+    )
+    
 }
