f046c4419cd8ee31f7ee1534a434c46b23b94d36
huzidev <huzaifa.iqdev@gmail.com>
2022-04-01 19:53:25 +0500

    updating counter

--- React/React-JS/Counter/src/MyComponents/App.js
+++ React/React-JS/Counter/src/MyComponents/App.js
@@ -3,6 +3,7 @@ import Counter from './Counter';
 
 export default function App() {
 
+    const [Count, SetCount] = React.useState(0); // 0 is the by default value or BEGINNING value
 
     
   return (
