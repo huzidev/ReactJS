f73b74b08531e8468d7ef893b36eb44e10d33b5b
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 01:27:32 +0500

    OnClick Object Completed

--- React/React-JS/UseState/OnClickObject/src/MyComponents/App.js
+++ React/React-JS/UseState/OnClickObject/src/MyComponents/App.js
@@ -14,14 +14,14 @@ export default function App() {
 
     })
 
-    let star = Contact.isFav ? "StarF.jpg" : "StarE.png" // as we know that ? and : are ternary operators ? means if true and : means else if false
+    let star = Contact.isFav ? "StarF.jpg" : "StarE.png" // as we know that ? and : are ternary operators ? means if true and : means else if false we'll pass this variable for src portion in ours JSX
 
     function Toggle() {
 
         SetContact(PrevContact => ({
 
-            ...Contact,
-            isFav: !PrevContact.isFav
+            ...Contact, // this is array spread operator which basically imports all the data of object
+            isFav: !PrevContact.isFav // ! means not hence ! not prevcontact.isfav means previously it is false by default now every time we click make it ! or OPPOSITE IN BRIEF
 
         })
 
@@ -37,19 +37,24 @@ export default function App() {
 
             <div className="card-info">
 
-                <img 
-                src={`../assets/${star}`}
-                className="fvr" 
-                onClick={Toggle}
-                alt="Star-Img" />
+                <img src={`../assets/${star}`} className="fvr" onClick={Toggle} alt="Star-Img" />
+                
                 <h2 className="card-name">
+
                     {Contact.firstname} {Contact.nickname}
+
                 </h2>
+
                 <p className="card-contact">
+
                     {Contact.phone}
+
                 </p>
+
                 <p className="card-contact">
+
                     {Contact.email}
+                    
                 </p>
 
             </div>
