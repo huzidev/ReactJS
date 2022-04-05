456ad9761dfa1608e6cc2c82aead23f2f0ab5be6
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 00:59:15 +0500

    user data

---
+++ React/React-JS/UseState/OnClickObject/src/MyComponents/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+
+  return (
+    const [Contact, SetContact] = React.useState({
+        firstname: "Huzi",
+        nickname: "Huzi-Dev",
+        phone: "1234567",
+        email: "huzi.iqdev@gmail.com",
+        isFav: false
+    })
+  )
+  
+}
