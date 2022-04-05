c67033338d1990b8403b816b78e400d1cec6a51e
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 01:06:16 +0500

    images added

--- React/React-JS/UseState/OnClickObject/src/MyComponents/App.js
+++ React/React-JS/UseState/OnClickObject/src/MyComponents/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default function App() {
 
-  return (
     const [Contact, SetContact] = React.useState({
         firstname: "Huzi",
         nickname: "Huzi-Dev",
@@ -10,6 +9,33 @@ export default function App() {
         email: "huzi.iqdev@gmail.com",
         isFav: false
     })
+
+  return (
+
+    <div className="main">
+
+        <div className="card">
+            <img src="../assets/main.jpg" alt="User-Img" />
+            <div className="card-info">
+                <img 
+                src={`../assets/StarE.png`}
+                className="fvr" 
+                alt="Star-Img" />
+                <h2 className="card-name">
+                    {Contact.firstname} {Contact.nickname}
+                </h2>
+                <p>
+                    {Contact.phone}
+                </p>
+                <p>
+                    {Contact.email}
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+
   )
-  
+
 }
