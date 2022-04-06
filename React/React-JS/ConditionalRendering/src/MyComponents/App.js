b91655e7b22f6c1c87780efd5f54391dbe4df74a
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:45:08 +0500

    import info React

--- React/React-JS/ConditionalRendering/src/MyComponents/App.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Data from './Data';
+import Info from './Info';
 
 export default function App() {
+
+    const JokeElements = Data.map(info => {
+
+        return (
+            <Info
+                key={info.id}
+                
+            />
+        )
+
+    })
 
   return (
   )
