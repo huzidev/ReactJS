e2df726340aff670bbfda77132a99c093235f86f
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:39:37 +0500

    data added

--- React/React-JS/ConditionalRendering/src/MyComponents/data.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/data.js
@@ -1,48 +1,29 @@
-// suppose this is the data from web-page we are working on now data will be updated as user insert their data into web-page
 
-// for that we don't have to actually update each time again and again rather we'll simply use mapping
-
-// THIS IS THE COMPONENT WHERE WE'LL RECEIVE OURS USER INPUT DATA WE'LL LINK IT TO APP FORM WHERE IT'LL BE LINK WITH CONTACT THROUGH MAPPING SO WHEN WE RECEIVE MOR AND MORE USER DATA WE DONT HAVE TO UPDATE EACH TIME RATHER MAPPING WILL DO THIS FOR US
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [
+export default[
     {
-        Img: "main.jpg",
-        title: "Learning ReactJS Mapping",
-        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
-        experience: "5+months",
-        stats: {
-            Languages: 7,    
-            Favorites: 6
-        },
-        Source: "Self-Taught",
-        HoursSpend: 5,
-        Power: "Max"
+        id: 1,
+        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
+        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
     },
     {
-        Img: "slider.jpg",
-        title: "Learning ReactJS Mapping",
-        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
-        experience: "5+months",
-        stats: {
-            Languages: 7,    
-            Favorites: 6
-        },
-        Source: "Self-Taught",
-        HoursSpend: 5,
-        Power: "Not-Max"
+        id: 2,
+        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
+        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
     },
     {
-        Img: "slider3.jpg",
-        title: "Learning ReactJS Mapping",
-        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
-        experience: "5+months",
-        stats: {
-            Languages: 7,    
-            Favorites: 6
-        },
-        Source: "Self-Taught",
-        HoursSpend: 5,
-        Power: "Not-Max"
-    }
+        id: 3,
+        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
+        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+    },
+    {
+        id: 4,
+        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
+        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+    },
+    {
+        id: 5,
+        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
+        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+    },
+
 ]
