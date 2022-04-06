3e55d87aa1f20e5ed476933103829b557167b24a
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:50:25 +0500

    toggle function

--- React/React-JS/ConditionalRendering/src/MyComponents/Info.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/Info.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-export default function Info() {
-  return (
-      
-  )
+export default function Info(props) {
+
+    return (
+        <div className="main">
+            <h3>
+                {props.setup}
+            </h3>
+            <p>
+                {props.punchline}
+            </p>
+            <button>
+                PunchLine
+            </button>
+            <hr />
+        </div>
+    )
+
 }
