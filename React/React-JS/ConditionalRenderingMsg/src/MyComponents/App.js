f3155dc3faea25f8c878cb37048c3f1b2b5f2edd
huzidev <huzaifa.iqdev@gmail.com>
2022-04-07 00:33:16 +0500

    algorithm for singular plural

--- React/React-JS/ConditionalRenderingMsg/src/MyComponents/App.js
+++ React/React-JS/ConditionalRenderingMsg/src/MyComponents/App.js
@@ -3,20 +3,24 @@ import React from 'react';
 export default function App() {
     
     const[message, func] = React.useState(["a", "b"])
-  
-    function alert() {
-        
-    }
 
     return (
 
         <div>
-            <h2>
-                Alert!
-            </h2>
-            <h5>
-                You've No Messages
-            </h5>
+            { 
+                message.length === 0 ?
+                <h5>
+                    You've No Messages
+                </h5> :
+                <div>
+                    <h2>
+                        Alert!
+                    </h2>
+                    <h5>
+                        You've {message.length} Unread 
+                    </h5>
+                </div>
+            }
         </div>
 
     )
