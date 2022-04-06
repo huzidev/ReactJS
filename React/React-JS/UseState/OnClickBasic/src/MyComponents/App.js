814c174964365494a25a9cac6126b06294d81299
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 18:25:34 +0500

    button

--- React/React-JS/UseState/OnClickBasic/src/MyComponents/App.js
+++ React/React-JS/UseState/OnClickBasic/src/MyComponents/App.js
@@ -4,7 +4,26 @@ export default function App() {
 
     const[Value, Function] = React.useState(True)
 
+    function Changing() {
+        
+        Function(PrevValue => !PrevValue)
+
+    }
   return (
+
+      <div className="Main">
+
+          <div className="Button">
+
+              <h1>
+
+                  {Value ? "Click" : "Click-Again"}
+
+              </h1>
+              
+          </div>
+
+      </div>
 
   )
 }
