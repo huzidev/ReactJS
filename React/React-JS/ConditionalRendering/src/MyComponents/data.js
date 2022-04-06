4674d62a3be087ad754aea7f2dbf105679b0a359
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:26:06 +0500

    conditonal rendering

---
+++ React/React-JS/ConditionalRendering/src/MyComponents/data.js
@@ -0,0 +1,48 @@
+// suppose this is the data from web-page we are working on now data will be updated as user insert their data into web-page
+
+// for that we don't have to actually update each time again and again rather we'll simply use mapping
+
+// THIS IS THE COMPONENT WHERE WE'LL RECEIVE OURS USER INPUT DATA WE'LL LINK IT TO APP FORM WHERE IT'LL BE LINK WITH CONTACT THROUGH MAPPING SO WHEN WE RECEIVE MOR AND MORE USER DATA WE DONT HAVE TO UPDATE EACH TIME RATHER MAPPING WILL DO THIS FOR US
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        Img: "main.jpg",
+        title: "Learning ReactJS Mapping",
+        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
+        experience: "5+months",
+        stats: {
+            Languages: 7,    
+            Favorites: 6
+        },
+        Source: "Self-Taught",
+        HoursSpend: 5,
+        Power: "Max"
+    },
+    {
+        Img: "slider.jpg",
+        title: "Learning ReactJS Mapping",
+        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
+        experience: "5+months",
+        stats: {
+            Languages: 7,    
+            Favorites: 6
+        },
+        Source: "Self-Taught",
+        HoursSpend: 5,
+        Power: "Not-Max"
+    },
+    {
+        Img: "slider3.jpg",
+        title: "Learning ReactJS Mapping",
+        description: "Hello, I've just started ReactJS and I'm really enjoying writing code in React it is really interesting library of JS",
+        experience: "5+months",
+        stats: {
+            Languages: 7,    
+            Favorites: 6
+        },
+        Source: "Self-Taught",
+        HoursSpend: 5,
+        Power: "Not-Max"
+    }
+]
