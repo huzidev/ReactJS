6cb656a0b26e5ec41d17d6d25ff1b80e52f9f12f
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:51:42 +0500

    use state

--- React/React-JS/ConditionalRendering/src/MyComponents/Info.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/Info.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 export default function Info(props) {
 
+    const [IsShow, Func] = React.useState(false)
+
     return (
+
         <div className="main">
+
             <h3>
                 {props.setup}
             </h3>
+
             <p>
                 {props.punchline}
             </p>
+            
             <button>
                 PunchLine
             </button>
+
             <hr />
         </div>
+
     )
 
 }
