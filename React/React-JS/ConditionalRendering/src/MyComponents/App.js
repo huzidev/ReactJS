098019619548dffbf19d31b20e676e0e653e4186
huzidev <huzaifa.iqdev@gmail.com>
2022-04-07 00:06:38 +0500

    comment

--- React/React-JS/ConditionalRendering/src/MyComponents/App.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/App.js
@@ -4,7 +4,7 @@ import Info from './Info';
 
 export default function App() {
 
-    const InfoElements = Data.map(info => {
+    const InfoElements = Data.map(info => { // DATA file contains all the data of user's or required data we are mapping it so it'll became self-sustaining and we don't have to update it every time whenever user insert more data
 
         return (
 
