c830d35903f0da00065b5b18a32eeba192536549
huzidev <huzaifa.iqdev@gmail.com>
2022-04-06 23:41:13 +0500

    data added for React

--- React/React-JS/ConditionalRendering/src/MyComponents/data.js
+++ React/React-JS/ConditionalRendering/src/MyComponents/data.js
@@ -1,29 +1,30 @@
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default[
     {
         id: 1,
-        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
-        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+        setup: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!",
+        punchline: "Lorem ipsum dolor sit, amet consectetur adipisicing elit"
     },
     {
         id: 2,
-        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
-        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+        setup: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!",
+        punchline: "Lorem ipsum dolor sit, amet consectetur adipisicing elit"
     },
     {
         id: 3,
-        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
-        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+        setup: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!",
+        punchline: "Lorem ipsum dolor sit, amet consectetur adipisicing elit"
     },
     {
         id: 4,
-        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
-        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
+        setup: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!",
+        punchline: "Lorem ipsum dolor sit, amet consectetur adipisicing elit"
     },
     {
         id: 5,
-        setup: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!,
-        punchline: Lorem ipsum dolor sit, amet consectetur adipisicing elit
-    },
+        setup: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni totam, esse voluptas natus, minus fuga impedit neque ullam quaerat eos ea veniam quo mollitia aut nostrum optio facere ipsum exercitationem!",
+        punchline: "Lorem ipsum dolor sit, amet consectetur adipisicing elit"
+    }
 
 ]
