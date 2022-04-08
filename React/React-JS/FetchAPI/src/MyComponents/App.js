f872a20ff5e7e1c7c93bc8645f3980929fb79047
huzidev <huzaifa.iqdev@gmail.com>
2022-04-09 02:09:15 +0500

    use effect

--- React/React-JS/FetchAPI/src/MyComponents/App.js
+++ React/React-JS/FetchAPI/src/MyComponents/App.js
@@ -4,6 +4,8 @@ export default function App() {
     
     const[StarWarsData, SetStarWarsData] = React.useState({})
     
+    const[Count, SetCount] = React.useState(1) // we didn't starts from zero because there can be no info about any character at 0th
+    
 
     fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
