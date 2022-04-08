6c209d02d76820fa945fd8b2fd6fa16cef748c5c
huzidev <huzaifa.iqdev@gmail.com>
2022-04-09 02:08:00 +0500

    formated documents

--- React/React-JS/FetchAPI/src/MyComponents/App.js
+++ React/React-JS/FetchAPI/src/MyComponents/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 export default function App() {
+    
+    const[StarWarsData, SetStarWarsData] = React.useState({})
+    
 
     fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
         .then(data => )
 
-  return (
+    return(
 
-  )
-
+    )
 }
