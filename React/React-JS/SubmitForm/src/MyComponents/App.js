33c7f9e2c7e1cc850be3de3d2edcbb30d994dcd2
huzidev <huzaifa.iqdev@gmail.com>
2022-04-08 22:42:40 +0500

    form in Reacty

--- React/React-JS/SubmitForm/src/MyComponents/App.js
+++ React/React-JS/SubmitForm/src/MyComponents/App.js
@@ -4,6 +4,32 @@ export default function App() {
 
     return(
 
+        <form >
+            <input 
+                placeholder="Yours Email"
+                type="text"
+                name="Email"
+                onChange={HandleChange}
+                value={FormData.name} 
+            />
+
+            <input 
+                placeholder="Password"
+                type="password"
+                name="Password"
+                onChange={HandleChange}
+                value={FormData.name} 
+            />
+
+            <input 
+                placeholder="Confirm Password"
+                type="password"
+                name="ConfirmPassword"
+                onChange={HandleChange}
+                value={FormData.name} 
+            />
+        </form>
+
     )
-    
+
 }
