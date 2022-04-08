d2c1e3be4800189f5f32e297bf00f206036868fd
huzidev <huzaifa.iqdev@gmail.com>
2022-04-09 02:14:23 +0500

    Counter Works

--- React/React-JS/FetchAPI/src/MyComponents/App.js
+++ React/React-JS/FetchAPI/src/MyComponents/App.js
@@ -3,15 +3,31 @@ import React from 'react';
 export default function App() {
     
     const[StarWarsData, SetStarWarsData] = React.useState({})
-    
+
     const[Count, SetCount] = React.useState(1) // we didn't starts from zero because there can be no info about any character at 0th
     
 
     fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
-        .then(data => )
+        .then(data => SetStarWarsData(data))
 
+    function Next() {
+
+        SetCount(PrevCount => PrevCount + 1)
+
+    }
     return(
+        
+        <div>
+            <h2>
+                The Current Count is {Count}
+            </h2>
+            <button onClick={Next}>
+
+                Get Next Character
+                
+            </button>
+        </div>
 
     )
 }
