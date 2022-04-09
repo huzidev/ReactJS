2d86dd218e5ddbf10f5065d1dfdcc4bda67c4a4f
huzidev <huzaifa.iqdev@gmail.com>
2022-04-10 02:57:54 +0500

    render components

--- React/React-JS/DarkMode/src/MyComponents/App.js
+++ React/React-JS/DarkMode/src/MyComponents/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Main from './Main';
+import Navbar from './Navbar';
 
 export default function App() {
 
     return (
+
+        <div>
+
+            <Main />
+            <Navbar />
+
+        </div>
         
     )
 
