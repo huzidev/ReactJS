993f4d58fd34f92379d701e4a6a2cbe79ed363d6
huzidev <huzaifa.iqdev@gmail.com>
2022-04-10 03:10:52 +0500

    toggle works

--- React/React-JS/DarkMode/src/MyComponents/App.js
+++ React/React-JS/DarkMode/src/MyComponents/App.js
@@ -4,15 +4,23 @@ import Navbar from './Navbar';
 
 export default function App() {
 
+    const[DarkMode, SetDarkMode] = React.useState(true)
+
+    function ToggleDarkMode() {
+        
+        SetDarkMode(PrevMode => !PrevMode)
+
+    }
+
     return (
 
         <div>
 
-            <Main />
-            <Navbar />
+            <Main darkMode={DarkMode}/>
+            <Navbar toggleDarkMode={ToggleDarkMode}/>
 
         </div>
-        
+
     )
 
 }
