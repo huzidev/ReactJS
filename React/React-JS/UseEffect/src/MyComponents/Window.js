27bd542943eb12264246fffbd69057b708109fe5
huzidev <huzaifa.iqdev@gmail.com>
2022-04-09 23:56:41 +0500

    set windows width

--- React/React-JS/UseEffect/src/MyComponents/Window.js
+++ React/React-JS/UseEffect/src/MyComponents/Window.js
@@ -6,7 +6,7 @@ export default function Window() {
 
     const[Width, SetWidth] = React.useState(width) // and width is equal to window.innerwidth
 
-    React.useEffect(() => {
+    React.useEffect(() => { // while using useEffect some time memory loss could occur and example of that is that if we click button and HIDES window width in UI then in console we'll face and error regarding memory leak therefore we've to use RemoveEventListener for this
 
         function WindowWidth() {
 
@@ -15,8 +15,13 @@ export default function Window() {
 
         }
 
-        window.addEventListener("resize", WindowWidth) // event listener is used to add and event which will run every time ours window's width changes 
+        window.addEventListener("resize", WindowWidth) // event listener is used to add and event which will run every time ours window's width changes
+        
+        return function() {
+            
+            window.removeEventListener("resize", WindowWidth)
 
+        }
 
 
     })
