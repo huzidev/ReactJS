2b9a242db7a1b7a6e271b49a897ca8e72e50538a
huzidev <huzaifa.iqdev@gmail.com>
2022-04-10 00:05:04 +0500

    inner height and width fix

--- React/React-JS/UseEffect/src/MyComponents/Window.js
+++ React/React-JS/UseEffect/src/MyComponents/Window.js
@@ -4,6 +4,8 @@ export default function Window() {
     
     const[Width, SetWidth] = React.useState(window.innerWidth) // and width is equal to window.innerwidth
 
+    const[Height, SetHeight] = React.useState(window.innerHeight)
+
     React.useEffect(() => { // while using useEffect some time memory loss could occur and example of that is that if we click button and HIDES window width in UI then in console we'll face and error regarding memory leak therefore we've to use RemoveEventListener for this
 
         function WindowWidth() {
@@ -13,13 +15,24 @@ export default function Window() {
 
         }
 
+        function WindowHeight() {
+
+            SetHeight(window.innerHeight)
+
+
+        }
+
         window.addEventListener("resize", WindowWidth) // event listener is used to add and event which will run every time ours window's width changes
         
+        window.addEventListener("resize", WindowHeight)
+
         return function() {
             
             console.log("Memory Leak fix");
-            
+
             window.removeEventListener("resize", WindowWidth)
+
+            window.removeEventListener("resize", WindowHeight)
 
         }
 
@@ -31,12 +44,12 @@ export default function Window() {
         <div className="main">
 
             <h2>
-                Winodw Width: {Width}
+                Window Width: {Width}
             </h2>
 
-            {/* <h2>
-                Window Height:
-            </h2> */}
+            <h2>
+                Window Height: {Height}
+            </h2>
 
         </div>
     
