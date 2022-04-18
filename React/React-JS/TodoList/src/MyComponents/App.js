5c9a0be2510b030df91e7d9ecbb07be2a0c81c31
huzidev <huzaifa.iqdev@gmail.com>
2022-04-18 17:49:29 +0500

    onClick method

--- React/React-JS/TodoList/src/MyComponents/App.js
+++ React/React-JS/TodoList/src/MyComponents/App.js
@@ -107,13 +107,21 @@ export default function App() {
             :
 
             <div className="no-notes">
+
                 <h1>You have no notes</h1>
+
                 <button 
+
                     className="first-note" 
-                    onClick={createNewNote}
+
+                    onClick={createNewNote} // when there is no notes we'll print the button for creating new note
+
                 >
+
                     Create one now
+
                 </button>
+
             </div>
             
         }
