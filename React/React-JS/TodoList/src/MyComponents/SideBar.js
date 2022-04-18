d9ef5b3bcb506e62ad55d0bcfc9cf116184fbf33
huzidev <huzaifa.iqdev@gmail.com>
2022-04-18 18:24:38 +0500

    ternary operators

--- React/React-JS/TodoList/src/MyComponents/SideBar.js
+++ React/React-JS/TodoList/src/MyComponents/SideBar.js
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function Sidebar(props) {
 
-    const noteElements = props.notes.map((note, index) => ( // props.notes.map MEANS it is been taking some ARRAY of NOTES
+    const noteElements = props.notes.map((note, index) => ( // props.notes.map MEANS it is been taking some ARRAY of NOTES and mapping it into ours CONST variables called NoteElements then we'll assign this NoteElements in to bottom inside {}
 
         <div key={note.id}>
 
             <div
                 
                 className={`title ${
-
-                    note.id === props.currentNote.id ? "selected-note" : ""
+                    
+                    note.id === props.currentNote.id ? "selected-note" : "" // if note.id is === CurrentNote.id then ClassName will be selected-note and background will be specific as we've assigned it to selected note in ours CSS else : there will be no class 
 
                 }`}
 
-                onClick={() => props.setCurrentNoteId(note.id)}
+                onClick={() => props.setCurrentNoteId(note.id)} // ON ONCLICK it'll changes Current Note id to that notes id on which we've CLICKED so we the color of that note changes each time and we'll see all different text and information about that specific note
 
             >
 
@@ -41,6 +41,6 @@ export default function Sidebar(props) {
             {noteElements}
 
         </section>
-        
+
     )
 }
