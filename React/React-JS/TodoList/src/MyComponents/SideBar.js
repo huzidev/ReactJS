432896402d93f4bbeaf1c8d7cc544f4a4e5da7f1
huzidev <huzaifa.iqdev@gmail.com>
2022-04-18 18:18:34 +0500

    formated documents

--- React/React-JS/TodoList/src/MyComponents/SideBar.js
+++ React/React-JS/TodoList/src/MyComponents/SideBar.js
@@ -5,25 +5,42 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => ( // props.notes.map MEANS it is been taking some ARRAY of NOTES
 
         <div key={note.id}>
+
             <div
                 
                 className={`title ${
+
                     note.id === props.currentNote.id ? "selected-note" : ""
+
                 }`}
+
                 onClick={() => props.setCurrentNoteId(note.id)}
+
             >
+
                 <h4 className="text-snippet">Note {index + 1}</h4>
+
             </div>
+
         </div>
+
     ))
 
     return (
+
         <section className="pane sidebar">
+
             <div className="sidebar--header">
+
                 <h3>Notes</h3>
+
                 <button className="new-note" onClick={props.newNote}>+</button>
+
             </div>
+
             {noteElements}
+
         </section>
+        
     )
 }
