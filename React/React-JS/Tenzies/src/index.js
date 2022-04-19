3e2669dbe42a932ad7bf872aa57dc52b67e4a7ae
huzidev <huzaifa.iqdev@gmail.com>
2022-04-19 21:46:41 +0500

    basic tools

---
+++ React/React-JS/Tenzies/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './MyComponents/App';
+import './styles/style.css';
+
+
+ReactDOM.render(<App />, document.getElementById("head"))
