9e6f3d9be56c1abdfd810490c9023099a1e72250
huzidev <huzaifa.iqdev@gmail.com>
2022-04-22 20:41:16 +0500

    timer class

--- React/React-JS/Tenzies/src/MyComponents/App.js
+++ React/React-JS/Tenzies/src/MyComponents/App.js
@@ -194,7 +194,7 @@ export default function App() {
                 
                 </h1>
 
-                <h3>
+                <h3 className="timer">
                 
                     {Minutes < 10 ? "0" + Minutes : Minutes}:{Seconds < 10 ? "0" + Seconds : Seconds}
                     {/* So we can have 00:00 if we didn't use the above step then we'll have just 0:0 */}
