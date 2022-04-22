27ea25f5ee8ff0d1811a588236c2a22f024fc39c
huzidev <huzaifa.iqdev@gmail.com>
2022-04-23 00:42:45 +0500

    formated documnets

--- React/React-JS/Tenzies/src/MyComponents/Header.js
+++ React/React-JS/Tenzies/src/MyComponents/Header.js
@@ -35,22 +35,22 @@ export default function Header() {
 
     return(
 
-    <div className="header">
+        <div className="header">
 
-        <h1 className="title">
-                    
-            Tenzies
-                    
-        </h1>
+            <h1 className="title">
+                        
+                Tenzies
+                        
+            </h1>
 
-        <h3 className="timer">
-                    
-            {Minutes < 10 ? "0" + Minutes : Minutes}:{Seconds < 10 ? "0" + Seconds : Seconds}
-            {/* So we can have 00:00 if we didn't use the above step then we'll have just 0:0 */}
+            <h3 className="timer">
+                        
+                {Minutes < 10 ? "0" + Minutes : Minutes}:{Seconds < 10 ? "0" + Seconds : Seconds}
+                {/* So we can have 00:00 if we didn't use the above step then we'll have just 0:0 */}
 
-        </h3>
-                    
-    </div>
+            </h3>
+                        
+        </div>
 
     )
 
