cd62044ef2dc0fc49291179bb6c4ccacc427e4a4
huzidev <huzaifa.iqdev@gmail.com>
2022-04-23 00:42:04 +0500

    style components

--- React/React-JS/Tenzies/src/MyComponents/App.js
+++ React/React-JS/Tenzies/src/MyComponents/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dice from './Dice';
+import Header from './Header'
 import {nanoid} from "nanoid";
 import Confetti from "react-confetti";
 
@@ -203,22 +204,7 @@ export default function App() {
 
             {Tenzies && <Confetti />}
 
-            <div className="header">
-
-                <h1 className="title">
-                
-                    Tenzies
-                
-                </h1>
-
-                <h3 className="timer">
-                
-                    {Minutes < 10 ? "0" + Minutes : Minutes}:{Seconds < 10 ? "0" + Seconds : Seconds}
-                    {/* So we can have 00:00 if we didn't use the above step then we'll have just 0:0 */}
-
-                </h3>
-                
-            </div>
+            <Header />
 
 
             <p className="instructions">
--- React/React-JS/Tenzies/src/MyComponents/Header.js
+++ React/React-JS/Tenzies/src/MyComponents/Header.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
 export default function Header() {
-    
+
+    const [Seconds, SetSeconds] = React.useState(0)
+
+    const [Minutes, SetMinutes] = React.useState(0)
+
+
+    // WE'VE TO USE (USE EFFECT) for timer because we wanted ours timer to update every seconds and if we didn't use (DEPENDENCY ARRAY) in ours (USE EFFECT) it'll update automatically every seconds
+
+    React.useEffect(() => {
+
+        var timer = setInterval(() => {
+            
+            SetSeconds(PrevSec => PrevSec + 1)
+
+            if ( Seconds === 59 ) {
+
+                // if seconds reach 59 then update ours minute and start counting seconds again from 0
+
+                SetMinutes(PrevMinutes => PrevMinutes + 1)
+
+                SetSeconds(PrevSec => 0)
+                
+                // therefore here we've set ours SetSeconds to zero
+            }
+
+        }, 1000);
+
+        return ()=> clearInterval(timer)
+
+    }, [Seconds]) // here if we use dependency array it'll be stopped just after a second
+
+
     return(
 
     <div className="header">
