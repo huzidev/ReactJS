82f5acc06c07e56038b1b969d30395fb72f4ebc9
huzidev <huzaifa.iqdev@gmail.com>
2022-04-23 00:43:07 +0500

    comment out code

--- React/React-JS/Tenzies/src/MyComponents/Header.js
+++ React/React-JS/Tenzies/src/MyComponents/Header.js
@@ -9,28 +9,28 @@ export default function Header() {
 
     // WE'VE TO USE (USE EFFECT) for timer because we wanted ours timer to update every seconds and if we didn't use (DEPENDENCY ARRAY) in ours (USE EFFECT) it'll update automatically every seconds
 
-    React.useEffect(() => {
+    // React.useEffect(() => {
 
-        var timer = setInterval(() => {
+    //     var timer = setInterval(() => {
             
-            SetSeconds(PrevSec => PrevSec + 1)
+    //         SetSeconds(PrevSec => PrevSec + 1)
 
-            if ( Seconds === 59 ) {
+    //         if ( Seconds === 59 ) {
 
-                // if seconds reach 59 then update ours minute and start counting seconds again from 0
+    //             // if seconds reach 59 then update ours minute and start counting seconds again from 0
 
-                SetMinutes(PrevMinutes => PrevMinutes + 1)
+    //             SetMinutes(PrevMinutes => PrevMinutes + 1)
 
-                SetSeconds(PrevSec => 0)
+    //             SetSeconds(PrevSec => 0)
                 
-                // therefore here we've set ours SetSeconds to zero
-            }
+    //             // therefore here we've set ours SetSeconds to zero
+    //         }
 
-        }, 1000);
+    //     }, 1000);
 
-        return ()=> clearInterval(timer)
+    //     return ()=> clearInterval(timer)
 
-    }, [Seconds]) // here if we use dependency array it'll be stopped just after a second
+    // }, [Seconds]) // here if we use dependency array it'll be stopped just after a second
 
 
     return(
