c0dd37595a002926f1640eeca43ccef19b8d9d80
huzidev <huzaifa.iqdev@gmail.com>
2022-04-25 22:51:29 +0500

    margin added

--- React/React-JS/ReactRouter/src/MyComponents/Home.js
+++ React/React-JS/ReactRouter/src/MyComponents/Home.js
@@ -3,10 +3,16 @@ import Img from '../assets/slider.jpg'
 
 export default function Home() {
     return (
-        
+
         <div className='home'>
 
             <img src={Img} alt="Main-Img" className='home-img'/>
+            <h1>
+                Goku
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolore quis delectus iste repellendus, deserunt rerum nesciunt repellat optio autem aliquam eum ad sint, facilis consectetur eius odit id at.
+            </p>
         </div>
 
 
