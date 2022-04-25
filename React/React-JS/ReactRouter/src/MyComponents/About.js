a6321136005d9ae8224613f2808db92230e9ea4f
huzidev <huzaifa.iqdev@gmail.com>
2022-04-25 22:52:26 +0500

    about web-page

---
+++ React/React-JS/ReactRouter/src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Imgg from '../assets/slider3.jpg'
+
+export default function Home() {
+    return (
+
+        <div className='about'>
+
+            <img src={Imgg} alt="Main-Img" className='about-img'/>
+            <h1>
+                Goku
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolore quis delectus iste repellendus, deserunt rerum nesciunt repellat optio autem aliquam eum ad sint, facilis consectetur eius odit id at.
+            </p>
+        </div>
+
+      )
+}
