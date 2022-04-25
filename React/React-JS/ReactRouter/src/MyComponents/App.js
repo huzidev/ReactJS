f0922fe813d2de2977bf3b0093a07049962e7f8a
huzidev <huzaifa.iqdev@gmail.com>
2022-04-26 00:45:40 +0500

    comment

--- React/React-JS/ReactRouter/src/MyComponents/App.js
+++ React/React-JS/ReactRouter/src/MyComponents/App.js
@@ -13,15 +13,14 @@ import {
 export default function App() {
     return (
         <>
+        {/* USING EXACT PATH INSTEAD OF JUST PATH IS GOOD PRACTICE BECAUSE REACT DO NOT READ IT COMPLETELY RATHER REACT READS IT PARTIALLY THEREFORE WE'VE TO USE EXACT PATH */}
             <Router>
                 <Navbar />
-                <div>
-                    <Routes>
-                        <Route exact path="/" element={<Home />}/>
-                        <Route exact path="/About" element={<About />}/>
-                        <Route exact path="/Contact" element={<Contact />}/>
-                    </Routes>
-                </div>
+                <Routes>
+                    <Route exact path="/" element={<Home />}/>
+                    <Route exact path="/About" element={<About />}/>
+                    <Route exact path="/Contact" element={<Contact />}/>
+                </Routes>
             </Router>
         </>
       )
