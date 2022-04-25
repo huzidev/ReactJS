b31ec1450eeaabe773b1a3c421f1ec1644534087
huzidev <huzaifa.iqdev@gmail.com>
2022-04-25 22:34:36 +0500

    importing logo

--- React/React-JS/ReactRouter/src/MyComponents/App.js
+++ React/React-JS/ReactRouter/src/MyComponents/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Logo from '../public/assets/react.png'
 
 export default function App() {
     return (
         <nav>
-            
+            <img src={Logo} alt="React-img" />
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </nav>
       )
 }
