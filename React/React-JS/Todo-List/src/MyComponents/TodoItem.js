1730f05ef682ccc1e618665cc95c76bb6dbaa310
huzidev <huzaifa.iqdev@gmail.com>
2022-04-26 23:31:11 +0500

    onClick

--- React/React-JS/Todo-List/src/MyComponents/TodoItem.js
+++ React/React-JS/Todo-List/src/MyComponents/TodoItem.js
@@ -12,6 +12,7 @@ export default function TodoItem({todos, onDelete}) {
             {todos.desc}
         </p>
         <button className='btn btn-sm btn-danger' onClick={onDelete}>
+            {/* Since we are using Destructuring Method therefore we don't have to use props.onDelete */}
             Delete
         </button>
 
