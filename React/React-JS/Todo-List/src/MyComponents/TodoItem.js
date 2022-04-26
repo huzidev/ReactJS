b8f2c24348842ca6b5b736b72771a53ecae210f5
huzidev <huzaifa.iqdev@gmail.com>
2022-04-27 01:12:58 +0500

    onclick delete function

--- React/React-JS/Todo-List/src/MyComponents/TodoItem.js
+++ React/React-JS/Todo-List/src/MyComponents/TodoItem.js
@@ -15,6 +15,7 @@ export default function TodoItem({todos, onDelete}) {
             {/* Since we are using Destructuring Method therefore we don't have to use props.onDelete */}
             {/* IT IS MANDATORY to use arrow function so it can call back the complete function while deleting any specific list item */}
             {/* IF WE JUST SAYS onClick={onClick} this means we are just PASSING function BUT when we did this onClick={onClick()} now it means we are CALLING  the function */}
+            {/* FOR CALLING FUNCTION THE BEST WAY IS TO USE ARROW FUNCTION AND MAKE SURE THAT WE'LL PASS SAME PARAMETER AS WE ARE PASSING HE PARAMETER IN APP.JS where we've created ours onClick Delete function*/}
             Delete
         </button>
 
