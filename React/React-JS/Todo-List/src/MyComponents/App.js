119f8730aa17c07cf81fb33a812a5aefb722de77
huzidev <huzaifa.iqdev@gmail.com>
2022-04-26 23:18:47 +0500

    todos tittle added

--- React/React-JS/Todo-List/src/MyComponents/App.js
+++ React/React-JS/Todo-List/src/MyComponents/App.js
@@ -1,9 +1,32 @@
 import Header from './Header';
+import Todos from './Todos';
 
 function App() {
+
+  let todos = [
+    {
+      sno: 1,
+      tittle: "Go To University",
+      desc: "Go To University And Do Study And Meet Yours Friends",
+    },
+
+    {
+      sno: 2,
+      tittle: "Go To University",
+      desc: "Go To University And Do Study And Meet Yours Friends"
+    },
+
+    {
+      sno: 3,
+      tittle: "Go To University",
+      desc: "Go To University And Do Study And Meet Yours Friends"
+    }
+  ]
+
   return (
     <> 
       <Header tittle = "My Todos List" searchBar={false}/>
+      <Todos todos={todos}/>
     </>
   )
 }
--- React/React-JS/Todo-List/src/MyComponents/Todos.js
+++ React/React-JS/Todo-List/src/MyComponents/Todos.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-export const Todos = () => {
+export default function Todos() {
   return (
     <div>Todos</div>
   )
