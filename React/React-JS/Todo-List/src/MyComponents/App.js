8e177251408b315d552d515fd46874a31150a717
huzidev <huzaifa.iqdev@gmail.com>
2022-04-26 22:55:02 +0500

    buggs fix

--- React/React-JS/Todo-List/src/MyComponents/App.js
+++ React/React-JS/Todo-List/src/MyComponents/App.js
@@ -1,13 +1,10 @@
-import Header from './Header'; // we created my components folder and create a file calle Header in my components just like linking JS or CSS with HTML
-// make sure to close the name of file in return like we did here in return
+import Header from './Header';
 
 function App() {
   return (
-    // that's all JSX NOT HTML it's javascript syntax extension
-    // we've to wrap all the html written throught JSX in something if there is nothing then simply wrap it in empty as we did their
     <> 
       <Header tittle = "My Todos List" searchBar={false}/>
     </>
-  );
+  )
 }
 export default App;
