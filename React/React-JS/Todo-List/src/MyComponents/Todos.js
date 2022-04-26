2735f37ab5fba8f462186e62455c3b587336ca50
huzidev <huzaifa.iqdev@gmail.com>
2022-04-26 23:22:09 +0500

    style components

--- React/React-JS/Todo-List/src/MyComponents/Todos.js
+++ React/React-JS/Todo-List/src/MyComponents/Todos.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-export default function Todos() {
+export default function Todos(props) {
   return (
 
+    <div className='container'>
 
+      <h3 className='text-center my-3'>
+        Todos List
+      </h3>
+
+      {
+
+        props.todos.map(() => {
+
+          return <TodoItem />
+
+        })
+
+      }
+
+    </div>
 
   )
 }
