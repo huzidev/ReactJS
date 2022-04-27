b258018b6ebc5e3020b60c23aed6c3cd72fa5dfc
huzidev <huzaifa.iqdev@gmail.com>
2022-04-27 21:45:35 +0500

    JSX elements

--- React/React-JS/Todo-List/src/MyComponents/AddTodos.js
+++ React/React-JS/Todo-List/src/MyComponents/AddTodos.js
@@ -31,7 +31,7 @@ export default function AddTodos(props) {
 
         else{
 
-            addTodos(Tittle, Desc)
+            props.addTodos(Tittle, Desc)
 
         }
     }
@@ -42,20 +42,20 @@ export default function AddTodos(props) {
             Add Todo Items
         </h3>
         <form onSubmit={submit}>
-            <div class="mb-3">
-                <label htmlFor="tittle" class="mb-3">
+            <div className="mb-3">
+                <label htmlFor="tittle" className="mb-3">
                     Add Todo's Tittle
                 </label>
-                <input type="text" value={Tittle} onChange={(event) => SetTittle(event.target.value)} class="form-control" id="tittle" aria-describedby="emailHelp" placeholder="Tittle"/>
+                <input type="text" value={Tittle} onChange={(event) => SetTittle(event.target.value)} className="form-control" id="tittle" aria-describedby="emailHelp" placeholder="Tittle"/>
                 {/* REMEMBER once we provide the value it'll no longer be able to show us changes in the input tag THEREFORE we've to use onChange and then pass (event.target.value) SINCE we face this problem after PROVIDING VALUE therefore (event.target.value) */}
             </div>
-            <div class="mb-3">
-                <label htmlFor="tittle" class="mb-3">
+            <div className="mb-3">
+                <label htmlFor="tittle" className="mb-3">
                     Add Todo's Description
                 </label>
-                <input type="text" value={Desc} onChange={(event => SetDesc(event.target.value))} class="form-control" id="desc" placeholder="Description"/>
+                <input type="text" value={Desc} onChange={(event => SetDesc(event.target.value))} className="form-control" id="desc" placeholder="Description"/>
             </div>
-            <button type="submit" class="btn btn-sm btn-success">
+            <button type="submit" className="btn btn-sm btn-success">
                 Add Item
             </button>
         </form>
