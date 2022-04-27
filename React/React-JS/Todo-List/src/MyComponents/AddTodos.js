df7cd3e003c996261f25ef8f46514750d9b48b7e
huzidev <huzaifa.iqdev@gmail.com>
2022-04-27 21:40:36 +0500

    UI

--- React/React-JS/Todo-List/src/MyComponents/AddTodos.js
+++ React/React-JS/Todo-List/src/MyComponents/AddTodos.js
@@ -11,6 +11,12 @@ export default function AddTodos() {
         event.preventDefault()
         // so page will not reload
 
+        if (!Tittle && !Desc) {
+            
+            alert("Tittle And Description Can't Be Empty")
+
+        }
+
         if (!Tittle) {
             
             alert("Tittle Can't Be Empty")
@@ -29,12 +35,12 @@ export default function AddTodos() {
         <h3>
             Add Todo Items
         </h3>
-        <form>
+        <form onSubmit={submit}>
             <div class="mb-3">
                 <label htmlFor="tittle" class="mb-3">
                     Add Todo's Tittle
                 </label>
-                <input type="email" value={Tittle} onChange={(event) => SetTittle(event.target.value)} class="form-control" id="tittle" aria-describedby="emailHelp" placeholder="Tittle"/>
+                <input type="text" value={Tittle} onChange={(event) => SetTittle(event.target.value)} class="form-control" id="tittle" aria-describedby="emailHelp" placeholder="Tittle"/>
                 {/* REMEMBER once we provide the value it'll no longer be able to show us changes in the input tag THEREFORE we've to use onChange and then pass (event.target.value) SINCE we face this problem after PROVIDING VALUE therefore (event.target.value) */}
             </div>
             <div class="mb-3">
