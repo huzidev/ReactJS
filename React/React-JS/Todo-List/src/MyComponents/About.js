feb7e90ff1965a8b6e31f497009a4de9b503cb4b
huzidev <huzaifa.iqdev@gmail.com>
2022-04-28 00:36:09 +0500

    about web-page

---
+++ React/React-JS/Todo-List/src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='about'>
+        <h2>
+            About-WebPage
+        </h2>
+        <p>
+            This is simple basic react TodoList application which is Created With HTML, CSS, Bootstrap and ReactJS 
+        </p>
+    </div>
+  )
+}
