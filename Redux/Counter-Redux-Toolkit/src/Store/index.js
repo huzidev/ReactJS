1db5e62e1eddb423b650584a07b910ac573f4486
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 15:53:02 +0500

    initial state

--- Redux/Counter-Redux-Toolkit/src/Store/index.js
+++ Redux/Counter-Redux-Toolkit/src/Store/index.js
@@ -1,1 +1,12 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// instead of {createStore} we've configureStore in Redux-toolkit
+
+// create-slice will take slice name and STATES AND REDUCERS as object for all the functions because REDUCERS is used for performing function
+
+const counterSlice = createSlice({
+
+    name : 'counter',
+    initialState = 
+
+})
