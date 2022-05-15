404c58ef612873ebf6695eb45465cae87f434600
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 17:36:27 +0500

    login page

--- Redux/Shopping-App/src/MyComponents/App.js
+++ Redux/Shopping-App/src/MyComponents/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function App() {
     return (
-        <div></div>
+        <div className='container'>
+            <h1>
+                Login
+            </h1>
+            <form>
+                <label htmlFor="id">
+                    Id
+                </label>
+                <input type="text" name='id' id='id' />
+                <label htmlFor="password">
+                    password
+                </label>
+                <input type="password" name='password' id='password' />
+            </form>
+        </div>
     )
 }
