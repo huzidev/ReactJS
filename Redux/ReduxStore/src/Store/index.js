a6c141988366b3d591a0bbb5fb78fcefbb44e0ea
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 12:48:14 +0500

    comments

--- Redux/ReduxStore/src/Store/index.js
+++ Redux/ReduxStore/src/Store/index.js
@@ -1,5 +1,10 @@
 import { createStore } from 'redux';
 
+
+// state is the form of state from any other component where we've created state and all the functions and receiving that state in this components so we can gave access to all other components this state values WITHOUT creating separate state for each components  and we can provide initial state like we did here that counter : 0
+
+// and action is the method though which we can dispatch (SEND) it to ours REDUCER to perform function
+
 const reducerFn = (state = { counter : 0 }, action) => {
 
 
