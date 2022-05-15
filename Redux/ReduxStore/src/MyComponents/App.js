e005f96ab2758235d3b57257c87348286c022abd
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 13:15:41 +0500

    dispatch

--- Redux/ReduxStore/src/MyComponents/App.js
+++ Redux/ReduxStore/src/MyComponents/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'; 
 
+// useSelector will takes and State which we've provided in ours REDUX-STORE and that state have an object equals to {counter : 0} therefore we've used state.counter
+
+//useSelector will always takes STATE as ARGUMENTS OR AS PARAMETER from ours REDUX-STORE if we hover on the useSelector we can see that
+
 export default function App() {
 
-
+    const counter = useSelector((state) => state.counter)
 
     return (
         <div>
