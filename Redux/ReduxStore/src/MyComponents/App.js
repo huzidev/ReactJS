608328defef90371b01687a48849dfbb46ccbd27
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 13:23:29 +0500

    redux store

--- Redux/ReduxStore/src/MyComponents/App.js
+++ Redux/ReduxStore/src/MyComponents/App.js
@@ -6,6 +6,8 @@ import { useSelector, useDispatch } from 'react-redux';
 //useSelector will always takes STATE as ARGUMENTS OR AS PARAMETER from ours REDUX-STORE if we hover on the useSelector we can see that
 
 // DISPATCH is just a function used to Send ours ACTIONS TO REDUCER if we hover on the useDispatch we can see the uses of dispatch
+
+// and we'll used that DISPATCH in ours FUNCTION where we wanted to import or wanted to have the access of state ACTION thought REDUX-STORE
 export default function App() {
 
     const counter = useSelector((state) => state.counter);
