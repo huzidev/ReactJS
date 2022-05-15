40931f853f69965d48b18a6bcb2f8328f8141c5c
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 17:49:05 +0500

    margin padding fix

--- Redux/Shopping-App/src/MyComponents/Auth.js
+++ Redux/Shopping-App/src/MyComponents/Auth.js
@@ -10,11 +10,15 @@ export default function Auth() {
                 <label htmlFor="id">
                     Id
                 </label>
+                <br />
                 <input type="text" name='id' id='id' />
+                <br />
                 <label htmlFor="password">
                     password
                 </label>
+                <br />
                 <input type="password" name='password' id='password' />
+                <br />
                 <button className='login-btn' type='submit'>
                     Login
                 </button>
