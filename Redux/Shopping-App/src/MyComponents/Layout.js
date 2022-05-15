c27b0cc9d70fcca4c94a9197ac90991c64ca4f06
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 18:18:48 +0500

    cart logo

---
+++ Redux/Shopping-App/src/MyComponents/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Auth() {
+    return (
+        <div>
+            <nav>
+                <h2>
+                    Redux Shopping App
+                </h2>
+                <div className='cart'>
+                    <h5>
+                        cart: 5 items
+                    </h5>
+                </div>
+            </nav>
+        </div>
+    )
+}
