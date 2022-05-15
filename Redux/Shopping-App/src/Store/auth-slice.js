10b82fb2d44a4aebf2e99a0da3aad4a392b655f7
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 23:03:52 +0500

    createSlice

--- Redux/Shopping-App/src/Store/auth-slice.js
+++ Redux/Shopping-App/src/Store/auth-slice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+
+    name : "auth",
+    initialState : { isLoggedIn },
+    reducers : {
+
+        
+
+    }
+
+})
