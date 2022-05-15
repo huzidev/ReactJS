716aa2762214d1df5d1180eae1998931b525c897
huzidev <huzaifa.iqdev@gmail.com>
2022-05-15 13:20:05 +0500

    redux state

--- Redux/ReduxStore/src/MyComponents/App.js
+++ Redux/ReduxStore/src/MyComponents/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'; 
 
-// useSelector will takes and State which we've provided in ours REDUX-STORE and that state have an object equals to {counter : 0} therefore we've used state.counter
+// useSelector will takes a State which we've provided in ours REDUX-STORE and that state have an object equals to {counter : 0} therefore we've used state.counter
 
 //useSelector will always takes STATE as ARGUMENTS OR AS PARAMETER from ours REDUX-STORE if we hover on the useSelector we can see that
 
