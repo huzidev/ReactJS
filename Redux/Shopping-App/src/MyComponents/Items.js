1a7290f283c9f9ca6e735da46d49bda38125d43c
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 16:17:35 +0500

    errors fixed

--- Redux/Shopping-App/src/MyComponents/Items.js
+++ Redux/Shopping-App/src/MyComponents/Items.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import products from './products';
 
+// MAKE SURE TO MAKE SEPARATE COMPONENTS FOR YOURS PRODUCTS ITEMS
+// while using map because else wise all the items will print separately according to the quantity of the data in products.js like here we've total 5 products lists in ours products.js if i make map method in app.js then pass the props and print it then it'll print all the 5 diff products list differently
+
 export default function items() {
 
     const list = products.map((info) => {
