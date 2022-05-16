8c9478fb271fe880ddcb7809da0869ed5cf41e3e
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 23:16:08 +0500

    destructuring

--- Redux/Shopping-App/src/MyComponents/cartItem.js
+++ Redux/Shopping-App/src/MyComponents/cartItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function cartItem() {
+export default function cartItem({Id, name, quantity, total, price}) {
     return (
         <div>
-            
+            <h2>
+
+            </h2>
         </div>
     )
 }
