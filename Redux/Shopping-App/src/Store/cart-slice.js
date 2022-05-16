a9e933adfb2b5f0ac20df2cdc3735d3b145e5461
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 17:15:33 +0500

    cart items

--- Redux/Shopping-App/src/Store/cart-slice.js
+++ Redux/Shopping-App/src/Store/cart-slice.js
@@ -1,1 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
+
+const createSlice = createSlice({
+
+    name : "Cart",
+    initialState : {
+
+        itemsList : [], // because array can store multiple values
+        totalQuantity : 0, // by default it should've to be zero
+
+    }
+
+})
