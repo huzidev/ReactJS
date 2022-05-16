ae01d0d6ae0afebd81350f6c3ecdf477d7bb8d9e
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 12:15:37 +0500

    data for items

--- Redux/Shopping-App/src/MyComponents/products.js
+++ Redux/Shopping-App/src/MyComponents/products.js
@@ -1,32 +1,33 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
     {
         Id: 1,
         name: "Laptop",
         imgURL: "item.jfif",
-        price: $ 25
+        price: "$ 25"
     },
     {
         Id: 2,
         name: "Laptop",
         imgURL: "item.jfif",
-        price: $ 25
+        price: "$ 25"
     },
     {
         Id: 3,
         name: "Laptop",
         imgURL: "item.jfif",
-        price: $ 25
+        price: "$ 25"
     },
     {
         Id: 4,
         name: "Laptop",
         imgURL: "item.jfif",
-        price: $ 25
+        price: "$ 25"
     },
     {
         Id: 5,
         name: "Laptop",
         imgURL: "item.jfif",
-        price: $ 25
+        price: "$ 25"
     }
 ]
