733d18e20e8249dab71fe1bf2785e96eb01ef432
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 18:39:31 +0500

    useSelector state

--- Redux/Shopping-App/src/MyComponents/App.js
+++ Redux/Shopping-App/src/MyComponents/App.js
@@ -7,6 +7,8 @@ export default function App() {
 
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
+    const cartItems = useSelector(state => state.cart.itemsList);
+
 // useSelector is used for have the access of REDUX-STORE and takes Redux-state as parameter and (State) is basically REDUX's state and auth is the name we've provided in ours REDUX STORE inside REDUCER and isLoggedIn is the action we wanted to update
 
     return (
