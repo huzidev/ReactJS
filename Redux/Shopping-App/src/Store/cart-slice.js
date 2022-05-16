b6dc33f7eeb9188a16126ee2caa7be5b495d25d6
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 17:18:45 +0500

    parameters

--- Redux/Shopping-App/src/Store/cart-slice.js
+++ Redux/Shopping-App/src/Store/cart-slice.js
@@ -8,9 +8,11 @@ const createSlice = createSlice({
         itemsList : [], // because array can store multiple values
         totalQuantity : 0, // by default it should've to be zero
         showCart : false
-    }
+    },
     reducers : {
 
+        // addToCart will takes state and action so when user will click on addToCart the action will update accordingly
+        
         addToCar(state, action) {
 
         },
@@ -19,7 +21,7 @@ const createSlice = createSlice({
 
         setShowCart(state) {
 
-            state.showCart = true
+            state.showCart = true;
 
         }
     }
