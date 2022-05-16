c3b1b8748da6f60fb7a34f0afd9f7dd3339e05b1
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 17:27:21 +0500

    find function

--- Redux/Shopping-App/src/Store/cart-slice.js
+++ Redux/Shopping-App/src/Store/cart-slice.js
@@ -17,7 +17,11 @@ const createSlice = createSlice({
 
             const newItem = action.payload; // which is going to be according to user that how much time user clicked on addToCart
 
-            // TO CHECK IF ANY ITEM IS ALREADY PRESENT
+            // TO CHECK IF ANY ITEM IS ALREADY PRESENT mens if user wanted to purchase the same product MULTIPLE TIMES
+
+            const existingItem = state.itemsList.find((item) => item.id === newItem.id)
+
+            // means if we've laptop of dell with id 1 and user wanted to add same laptop once again then state.itemList.find() means to run like a loop to check the itemsList which is an array until we found item.id which is equals to newItem because user wanted to add same product once again
 
         },
 
