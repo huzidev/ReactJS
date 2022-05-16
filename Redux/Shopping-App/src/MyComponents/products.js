cfab4765ad6ed756f8444345fcb9880827152d42
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 12:13:45 +0500

    products data added

---
+++ Redux/Shopping-App/src/MyComponents/products.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        Id: 1,
+        name: Laptop,
+        imgURL: "item.jfif",
+        price: $ 25
+    },
+    {
+        Id: 2,
+        name: Laptop,
+        imgURL: "item.jfif",
+        price: $ 25
+    },
+    {
+        Id: 3,
+        name: Laptop,
+        imgURL: "item.jfif",
+        price: $ 25
+    },
+    {
+        Id: 4,
+        name: Laptop,
+        imgURL: "item.jfif",
+        price: $ 25
+    },
+    {
+        Id: 5,
+        name: Laptop,
+        imgURL: "item.jfif",
+        price: $ 25
+    }
+]
