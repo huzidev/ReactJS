c4d367269c273d76fdb7ab51edbaaf94fffee9be
huzidev <huzaifa.iqdev@gmail.com>
2022-05-16 17:17:27 +0500

    reducers added

--- Redux/Shopping-App/src/Store/cart-slice.js
+++ Redux/Shopping-App/src/Store/cart-slice.js
@@ -7,7 +7,21 @@ const createSlice = createSlice({
 
         itemsList : [], // because array can store multiple values
         totalQuantity : 0, // by default it should've to be zero
+        showCart : false
+    }
+    reducers : {
 
+        addToCar(state, action) {
+
+        },
+
+        removeFromCart() {},
+
+        setShowCart(state) {
+
+            state.showCart = true
+
+        }
     }
 
 })
