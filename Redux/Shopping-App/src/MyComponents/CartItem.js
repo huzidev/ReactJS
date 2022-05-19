e45e1c433793bc5b3cc7b382b146033bf0cf55b7
huzidev <huzaifa.iqdev@gmail.com>
2022-05-19 18:55:13 +0500

    props method

--- Redux/Shopping-App/src/MyComponents/CartItem.js
+++ Redux/Shopping-App/src/MyComponents/CartItem.js
@@ -3,6 +3,15 @@ import React from 'react';
 export default function cartItem({Id, name, quantity, price, total}) {
     return (
         <div>
+            <h2>
+                {name}
+            </h2>
+            <p>
+                $ {price}
+            </p>
+            <p>
+                x {quantity}
+            </p>
             
         </div>
     )
