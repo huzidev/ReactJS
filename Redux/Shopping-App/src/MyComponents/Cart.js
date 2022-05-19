74a901026a78a8e02ab643c5b8daf140f78558e7
huzidev <huzaifa.iqdev@gmail.com>
2022-05-19 17:15:19 +0500

    cart btn

---
+++ Redux/Shopping-App/src/MyComponents/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Cart() {
+    return (
+        <div>
+            <div className='cart'>
+                <h5>
+                    cart: {quantity} items
+                </h5>
+            </div>
+        </div>
+    )
+}
