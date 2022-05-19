af74b5da749e220c3a4c0b963b745bd5487ff9f8
huzidev <huzaifa.iqdev@gmail.com>
2022-05-19 17:21:48 +0500

    useSelector hook

--- Redux/Shopping-App/src/MyComponents/CartItem.js
+++ Redux/Shopping-App/src/MyComponents/CartItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function cartItem() {
     return (
-        <div>cartItem</div>
+        <div>
+            
+        </div>
     )
 }
--- Redux/Shopping-App/src/MyComponents/CartItems.js
+++ Redux/Shopping-App/src/MyComponents/CartItems.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import cartItem from './cartItem';
+import { useSelector } from 'react-redux';
 
 export default function CartItems() {
+
+    
+
     return (
         <div>
-            
+
         </div>
       )
 }
