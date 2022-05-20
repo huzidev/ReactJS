45297a9ecf836e87fd2a134af7d9c471ff27e821
huzidev <huzaifa.iqdev@gmail.com>
2022-05-20 19:51:38 +0500

    severity for MIU

--- Redux/Shopping-App/src/MyComponents/Notification.js
+++ Redux/Shopping-App/src/MyComponents/Notification.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Alert } from '@mui/material';
 
-export default function Notification() {
+export default function Notification({type, message}) {
     return (
-        <div></div>
+        <div>
+            <Alert severity={type}>
+                {message}
+            </Alert>
+        </div>
     )
+//The severity of the alert. This defines the color and icon used according to type
 }
