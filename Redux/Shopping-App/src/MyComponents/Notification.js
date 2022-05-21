0c636393d22ed6ed45821f53017016057f80413a
huzidev <huzaifa.iqdev@gmail.com>
2022-05-21 18:20:26 +0500

    use Dispatch hook

--- Redux/Shopping-App/src/MyComponents/Notification.js
+++ Redux/Shopping-App/src/MyComponents/Notification.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Alert } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Notification({type, message}) {
+
+    const dispatch = useDispatch();
 
     const notification = useSelector((state) => state.ui.notification);
 
