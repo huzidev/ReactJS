5d5e4a2c713051b7f2b9c8682def39fbba6cd035
huzidev <huzaifa.iqdev@gmail.com>
2022-05-24 16:53:13 +0500

    setting up login

--- Redux/Redux-WebApp/src/MyComponents/Login.js
+++ Redux/Redux-WebApp/src/MyComponents/Login.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Login() {
     return (
         <div className='login-main'>
-
+            <div className='login-setup'>
+                
+            </div>
         </div>
     )
 }
