7bcbe465f94c3eea25661d7723cd2dfc63a81904
huzidev <huzaifa.iqdev@gmail.com>
2022-05-25 22:47:43 +0500

    reducer for Redux

--- Redux/Redux-WebApp/src/index.js
+++ Redux/Redux-WebApp/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./MyComponents/App";
 import "./styles/style.css";
 import "./styles/Login.css"
-// import { Provider } from 'react-redux';
-// import store from './Store';
+import { Provider } from 'react-redux';
+import store from './Store';
 
 ReactDOM.render(
-//    <Provider store={store}>
-        <App />,
-    // </Provider>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("head")
 )
