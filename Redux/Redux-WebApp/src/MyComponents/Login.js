0f5a8d6e9737d976d86a56a2ba64d44378d8ee81
huzidev <huzaifa.iqdev@gmail.com>
2022-05-25 16:44:48 +0500

    input tag

--- Redux/Redux-WebApp/src/MyComponents/Login.js
+++ Redux/Redux-WebApp/src/MyComponents/Login.js
@@ -4,7 +4,21 @@ export default function Login() {
     return (
         <div className='login-main'>
             <div className='login-setup'>
-                
+                <h2>
+                    Welcome
+                </h2>
+                <div>
+                    <h5>
+                        Username
+                    </h5>
+                    <input type="text" className='input'/>
+                </div>
+                <div>
+                    <h5>
+                        Password
+                    </h5>
+                    <input type="password" className='input'/>
+                </div>
             </div>
         </div>
     )
