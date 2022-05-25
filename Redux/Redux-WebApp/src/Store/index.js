e3df19a536eaefd705cda06b54901d702307723c
huzidev <huzaifa.iqdev@gmail.com>
2022-05-25 22:47:01 +0500

    export store

--- Redux/Redux-WebApp/src/Store/index.js
+++ Redux/Redux-WebApp/src/Store/index.js
@@ -1,2 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import loginStore from "./Login-Store.js"
+import loginStore from "./Login-Store.js";
+
+
+const store = configureStore({
+
+    reducer : {
+        login : loginStore.reducer
+    }
+
+})
+
+export default store;
