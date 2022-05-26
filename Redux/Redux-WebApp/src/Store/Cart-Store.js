4653c587df1599c2a5678c30b536626bee2bc0f4
huzidev <huzaifa.iqdev@gmail.com>
2022-05-26 17:22:56 +0500

    main redux sotre

--- Redux/Redux-WebApp/src/Store/Cart-Store.js
+++ Redux/Redux-WebApp/src/Store/Cart-Store.js
@@ -6,6 +6,14 @@ const cartStore = createSlice({
     initialState : {
         itemsList : [],
         totalQuantity : 0,
+    },
+    reducers : {
+        addToCart() {
+            
+        }
     }
+});
 
-})
+export const cartActions = cartStore.actions;
+
+export default cartStore;
--- Redux/Redux-WebApp/src/Store/index.js
+++ Redux/Redux-WebApp/src/Store/index.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logInStore from "./Login-Store.js";
 import layoutStore from "./Layout-Store.js";
+import cartStore from "./Cart-Store.js";
 
 
 const store = configureStore({
 
     reducer : {
         login : logInStore.reducer,
-        layout : layoutStore.reducer
+        layout : layoutStore.reducer,
+        cart : cartStore.reducer
     }
 
 });
