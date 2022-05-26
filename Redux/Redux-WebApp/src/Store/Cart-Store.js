2b24c5d75540992ff151cb61f2d33e4239e4e493
huzidev <huzaifa.iqdev@gmail.com>
2022-05-26 17:20:30 +0500

    array items

--- Redux/Redux-WebApp/src/Store/Cart-Store.js
+++ Redux/Redux-WebApp/src/Store/Cart-Store.js
@@ -1,1 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const cartStore = createSlice({
+
+    name : 'cart',
+    initialState : {
+        itemsList : [],
+        totalQuantity : 0,
+    }
+
+})
