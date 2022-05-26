e059794fea7d4b8f85ddef93a385fc2844688e26
huzidev <huzaifa.iqdev@gmail.com>
2022-05-26 21:57:33 +0500

    arrow btns functions

--- Redux/Redux-WebApp/src/MyComponents/Arrow.js
+++ Redux/Redux-WebApp/src/MyComponents/Arrow.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function Arrow() {
+export default function Arrow({prevSlide, nextSlide}) {
     return (
         <div className='arrows'>
             <span className='prev' onClick={prevSlide}>
                 &#10094;
+            </span>
+            <span className='next' onClick={nextSlide}>
+                &#10095;
             </span>
         </div>
     )
