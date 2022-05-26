a5419be302aaf268b83205a089dbba0de006c2b1
huzidev <huzaifa.iqdev@gmail.com>
2022-05-26 22:35:21 +0500

    slider added

--- Redux/Redux-WebApp/src/MyComponents/sliderImage.js
+++ Redux/Redux-WebApp/src/MyComponents/sliderImage.js
@@ -21,21 +21,21 @@ export default [
   {
     title: "Fourth Slide",
     description: "This is the Fourth slider Image of our carousel",
-    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(133).jpg",
+    urls: First,
   },
   {
     title: "Fifth Slide",
     description: "This is the Fifth slider Image of our carousel",
-    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(147).jpg",
+    urls: Second,
   },
   {
     title: "Sixth Slide",
     description: "This is the Sixth slider Image of our carousel",
-    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(105).jpg",
+    urls: Third,
   },
   {
     title: "Seventh Slide",
     description: "This is the Seventh slider Image of our carousel",
-    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(102).jpg",
+    urls: First,
   },
 ];
