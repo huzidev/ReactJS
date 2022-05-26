b00f1ed94f1cfed54c2ffeb218b9d4f2968d7a3c
huzidev <huzaifa.iqdev@gmail.com>
2022-05-26 12:46:17 +0500

    hamburger menu

--- Redux/Redux-WebApp/src/Store/Layout-Store.js
+++ Redux/Redux-WebApp/src/Store/Layout-Store.js
@@ -1,1 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const layoutStore = createSlice({
+
+    name : 'layout',
+    initialState : {
+        hamburgerOpen : false
+    },
+    reducers : {
+
+
+
+    }
+
+
+});
