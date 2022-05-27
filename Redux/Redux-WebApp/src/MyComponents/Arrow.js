97a0cbd5fd5103609f26b76cbdfd5ecdac1a20f1
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 10:28:15 +0500

    class fix

--- Redux/Redux-WebApp/src/MyComponents/Arrow.js
+++ Redux/Redux-WebApp/src/MyComponents/Arrow.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Arrow({prevSlide, nextSlide}) {
   return (
     <div className="arrows">
-      <span className="prev" onClick={prevSlide}>
+      <span className="prev arrow" onClick={prevSlide}>
         &#10094;
       </span>
-      <span className="next" onClick={nextSlide}>
+      <span className="next arrow" onClick={nextSlide}>
         &#10095;
       </span>
     </div>
