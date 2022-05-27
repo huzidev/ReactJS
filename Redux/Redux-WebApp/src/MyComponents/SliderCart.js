214dfc42781e6c6cf11404efdeaa7095dc7c3bad
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 21:58:39 +0500

    useDispatch

--- Redux/Redux-WebApp/src/MyComponents/SliderCart.js
+++ Redux/Redux-WebApp/src/MyComponents/SliderCart.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import ProductList from './ProductList.js'
 
 export default function SliderCart() {
+
+    const Products = ProductList.map((info) => {
+
+        const {Id, name, price, img} = info
+
+        const dispatch = useDispatch();
+
+    })
+
     return (
         <div>
             <div className='info'>
@@ -8,6 +19,9 @@ export default function SliderCart() {
                     Now, Lets Buy <br />
                     SomeThing.
                 </h3>
+            </div>
+            <div>
+
             </div>
         </div>
     )
