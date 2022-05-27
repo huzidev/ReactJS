bfb7550f8ef543efec64bb0828b7b924301a1548
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 11:00:19 +0500

    slider store created

---
+++ Redux/Redux-WebApp/src/Store/Slider-Store.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sliderStore = createSlice({
+
+    name : 'slider',
+    initialState : {},
+    reducers : {
+
+    }
+
+})
+
+export const sliderStoreActions = sliderStore.actions;
+
+export default sliderStore;
