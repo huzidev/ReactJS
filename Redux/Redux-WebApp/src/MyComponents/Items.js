a12a9f8afacc2d98f5214661bb596c280b089413
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 18:10:04 +0500

    height width

--- Redux/Redux-WebApp/src/MyComponents/Items.js
+++ Redux/Redux-WebApp/src/MyComponents/Items.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import Img from '../assets/product.webp'
 
 export default function Items() {
     return (
         <div className='container'>
             <div className='list'>
-                <div className="items"></div>
-                <div className="items"></div>
-                <div className="items"></div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
             </div>
             <div className='list'>
-                <div className="items"></div>
-                <div className="items"></div>
-                <div className="items"></div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
+                <div className="items">
+                    <img src={Img} alt="item-img" className="img" />
+                </div>
             </div>
         </div>
     )
