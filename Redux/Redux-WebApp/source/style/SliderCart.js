043666e83bf86011ac0ff1fe5f957b2f13875aba
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 18:38:28 +0500

    header file added

--- Redux/Redux-WebApp/source/style/SliderCart.js
+++ Redux/Redux-WebApp/source/style/SliderCart.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default function SliderCart() {
     return (
         <div>
-    
+            <div className='info'>
+                <h3>
+                    Now, Lets Buy <br />
+                    SomeThing.
+                </h3>
+            </div>
         </div>
     )
 };
