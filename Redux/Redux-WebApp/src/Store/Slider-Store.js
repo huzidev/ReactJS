c0713661d4ff2a1135b72d60556370c0950a491c
huzidev <huzaifa.iqdev@gmail.com>
2022-05-27 11:43:12 +0500

    fixing erros

--- Redux/Redux-WebApp/src/Store/Slider-Store.js
+++ Redux/Redux-WebApp/src/Store/Slider-Store.js
@@ -1,13 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import sliderImage from "../MyComponents/sliderImage";
 
 const sliderStore = createSlice({
 
     name : 'slider',
-    initialState : {},
+    initialState : {
+        sliderImage : [sliderImage.length],
+        setActiveIndex : 0,
+        len : sliderImage - 1
+    },
     reducers : {
-
+        console.log(state.len)
     }
-
 })
 
 export const sliderStoreActions = sliderStore.actions;
--- Redux/Redux-WebApp/src/Store/index.js
+++ Redux/Redux-WebApp/src/Store/index.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import logInStore from "./Login-Store.js";
 import layoutStore from "./Layout-Store.js";
 import cartStore from "./Cart-Store.js";
+import sliderStore from "./Slider-Store.js";
 
 
 const store = configureStore({
@@ -9,7 +10,8 @@ const store = configureStore({
     reducer : {
         login : logInStore.reducer,
         layout : layoutStore.reducer,
-        cart : cartStore.reducer
+        cart : cartStore.reducer,
+        slider : sliderStore.reducer
     }
 
 });
