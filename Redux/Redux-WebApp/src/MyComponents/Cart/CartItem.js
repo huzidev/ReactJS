82f257122fef70fee6eb83f2a01466faddf7f5ed
huzidev <huzaifa.iqdev@gmail.com>
2022-05-28 19:06:55 +0500

    style components for list items

--- Redux/Redux-WebApp/src/MyComponents/Cart/CartItem.js
+++ Redux/Redux-WebApp/src/MyComponents/Cart/CartItem.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function () {
-  return (
-    <div></div>
-  )
+export default function CartItem() {
+    return (
+        <div>
+            
+        </div>
+    )
 }
