e6cf097a8bc63c70b8020bb36ac57013235eac27
huzidev <huzaifa.iqdev@gmail.com>
2022-05-28 15:47:41 +0500

    fixing deleteFromCart

--- Redux/Redux-WebApp/src/Store/Cart-Store.js
+++ Redux/Redux-WebApp/src/Store/Cart-Store.js
@@ -48,13 +48,13 @@ const cartStore = createSlice({
 
         },
 
-        delFromCart(state, action) {
+        // delFromCart(state, action) {
 
-            const Id = action.payload;
+        //     const Id = action.payload;
 
-            const existingItem = state.itemsList.find((item) => item.Id === Id);
+        //     const existingItem = state.itemsList.find((item) => item.Id === Id);
 
-        }
+        // }
     }
 });
 
