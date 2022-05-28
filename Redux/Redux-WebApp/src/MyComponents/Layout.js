9b0356e76b8cad5e6424159fd693150cd4a4d6b3
huzidev <huzaifa.iqdev@gmail.com>
2022-05-28 19:50:06 +0500

    fixing erros

--- Redux/Redux-WebApp/src/MyComponents/Layout.js
+++ Redux/Redux-WebApp/src/MyComponents/Layout.js
@@ -12,7 +12,7 @@ export default function Layout() {
 
   return (
     <div>
-      { setShowCart ? <CartItems /> : (
+      {setShowCart ? <CartItems /> : (
           <>
             <Nav />
             <Slider />
