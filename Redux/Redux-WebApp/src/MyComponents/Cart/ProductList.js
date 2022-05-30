0a3c108b7ac4cb9690b47c5fd8857a75a6d12f36
huzidev <huzaifa.iqdev@gmail.com>
2022-05-30 19:39:23 +0500

    redux web-store

--- Redux/Redux-WebApp/src/MyComponents/Cart/ProductList.js
+++ Redux/Redux-WebApp/src/MyComponents/Cart/ProductList.js
@@ -9,17 +9,17 @@ export default [
     {
         Id : 2,
         name : "Laptop",
-        img : "test.avif",
+        img : "product.webp",
         price : 25
         
-    }
-//     {
-//         Id : 3,
-//         name : "Laptop",
-//         img : "test.avif",
-//         price : 25
+    },
+    {
+        Id : 3,
+        name : "Laptop",
+        img : "second.webp",
+        price : 25
         
-//     },
+    },
 //     {
 //         Id : 4,
 //         name : "Laptop",
