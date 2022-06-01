395b8ca19d38602871522ad34f18a86b29e3f3f2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-01 17:43:14 +0500

    anchor tag

--- Redux/Redux-WebApp/src/MyComponents/Nav.js
+++ Redux/Redux-WebApp/src/MyComponents/Nav.js
@@ -27,7 +27,7 @@ export default function Nav() {
                 <div className='header-section logo-section'>
                     <img src={Logo} alt="logo" className="logo"/>
                 </div>
-                <div className='right'>
+                <div className={'ham-side-bar' + activeClass}>
                     <div className='cart-section'>
                         <div className='cart-btn'>
                             <i onClick={setShowCart} className="fas fa-shopping-cart"></i>
@@ -38,21 +38,26 @@ export default function Nav() {
                             </div>
                         </div>
                     </div>
-                    <div className={'ham-side-bar' + activeClass}>
-                        <div className='cart-section'>
-                            <div className='cart-btn'>
-                                <i onClick={setShowCart} className="fas fa-shopping-cart"></i>
-                                <div className='counter'>
-                                    <p>
-                                        {totalQuantity}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div onClick={hamburger} className={'ham' + activeClass}>
-                            <div className='hamburger-menu'>
-                                <div className='menu'>
-                                </div>
+                    <ul>
+                        <li className='nav-item'>
+                            <a href="#">
+                                About
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="#">
+                                Contact Us
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="#">
+                                More
+                            </a>
+                        </li>
+                    </ul>
+                    <div onClick={hamburger} className={'ham' + activeClass}>
+                        <div className='hamburger-menu'>
+                            <div className='menu'>
                             </div>
                         </div>
                     </div>
