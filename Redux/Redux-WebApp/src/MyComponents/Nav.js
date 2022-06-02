34770cdb13de1e32a99201f641d9904147e1cdc0
huzidev <huzaifa.iqdev@gmail.com>
2022-06-02 08:54:07 +0500

    drop-down items added

--- Redux/Redux-WebApp/src/MyComponents/Nav.js
+++ Redux/Redux-WebApp/src/MyComponents/Nav.js
@@ -72,6 +72,7 @@ export default function Nav() {
                                     <li>hello</li>
                                     <hr />
                                     <li>hello</li>
+                                    <hr />
                                 </ul>
                             </li>
                             <i className='fa fa-caret-down'>
