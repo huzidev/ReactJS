b72086ef29f1e666e95acffdc325677840bd4f59
huzidev <huzaifa.iqdev@gmail.com>
2022-06-03 18:58:30 +0500

    logos added

--- Redux/Redux-WebApp/src/MyComponents/Footer.js
+++ Redux/Redux-WebApp/src/MyComponents/Footer.js
@@ -9,15 +9,24 @@ export default function Footer() {
                 </h2>
                 <div className='footer-items'>
                     <div className="logo">
+                        <i className="fab fa-github">
+                        </i>
                         test
                     </div>
                     <div className="logo">
+                        <i className="fa fa-envelope">
+                        </i>
                         test
                     </div>
                     <div className="logo">
+                        <i className="fa fa-phone">
+                        </i>
                         test
                     </div>
                     <div className="logo">
+                        <i className="fa fa-instagram">
+
+                        </i>
                         test
                     </div>
                 </div>
