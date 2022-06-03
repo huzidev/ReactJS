d71b28bf7c383b3cb3b882b6f48a5f515a8d93bb
huzidev <huzaifa.iqdev@gmail.com>
2022-06-03 19:02:46 +0500

    text area added

--- Redux/Redux-WebApp/src/MyComponents/Footer.js
+++ Redux/Redux-WebApp/src/MyComponents/Footer.js
@@ -11,25 +11,48 @@ export default function Footer() {
                     <div className="logo">
                         <i className="fab fa-github">
                         </i>
-                        test
+                        <a href="">
+                            Huzi-Dev
+                        </a>
                     </div>
                     <div className="logo">
                         <i className="fa fa-envelope">
                         </i>
-                        test
+                        <a href="">
+                            Huzaifa Iqbal
+                        </a>
                     </div>
                     <div className="logo">
                         <i className="fa fa-phone">
                         </i>
-                        test
+                        <a href="">
+                            123-456-789
+                        </a>
                     </div>
                     <div className="logo">
                         <i className="fa fa-instagram">
-
                         </i>
-                        test
+                        <a href="">
+                            huzaifa_iqbal_
+                        </a>
                     </div>
                 </div>
+                <form action="">
+                    <div className='form-main'>
+                        <div className='form-side'>
+                            <input type="text" placeholder='Yours Name'/>
+                            <input type="email" placeholder='Yours Email' />
+                        </div>
+                        <div className='form-comp'>
+                            <textarea name="message" cols="30" rows="10" placeholder='Yours Message'>
+                                
+                            </textarea>
+                        </div>
+                        <button>
+                            Send Message
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
