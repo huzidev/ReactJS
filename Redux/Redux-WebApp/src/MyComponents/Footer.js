0f3b3053a5242d2f6324ce7680b101af55513201
huzidev <huzaifa.iqdev@gmail.com>
2022-06-03 18:45:32 +0500

    heading for footer

--- Redux/Redux-WebApp/src/MyComponents/Footer.js
+++ Redux/Redux-WebApp/src/MyComponents/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div>
-            
+        <div className='footer-container'>
+            <h2>
+                
+            </h2>
         </div>
     )
 }
