532dd3a52a9e6230c7776dc049f6fe2a8c858784
huzidev <huzaifa.iqdev@gmail.com>
2022-07-18 12:00:11 +0500

    props

--- Redux/coustom-data/src/components/Nav.js
+++ Redux/coustom-data/src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
         <div>
-            <a href="/owner">Owner</a>
+            <Link to='/owner'>owner info</Link> 
+            <Link to='/owner'>products</Link> 
         </div>
     )
 }
