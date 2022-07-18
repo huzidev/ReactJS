8042598cfd6369d9e08899bc14c0c2c3eb7974be
huzidev <huzaifa.iqdev@gmail.com>
2022-07-18 11:02:41 +0500

    git add .

--- Redux/coustom-data/src/components/data.js
+++ Redux/coustom-data/src/components/data.js
@@ -4,34 +4,39 @@ export default [
         name : "product-1",
         description : "first product nice one",
         price : 500,
-        model : 2022
+        model : 2022,
+        img : "product.webp"
     },
     {
         id : 1,
         name : "product-1",
         description : "first product nice one",
         price : 500,
-        model : 2022
+        model : 2022,
+        img : "second.webp"
     },
     {
         id : 1,
         name : "product-1",
         description : "first product nice one",
         price : 500,
-        model : 2022
+        model : 2022,
+        img : "test.avif"
     },
     {
         id : 1,
         name : "product-1",
         description : "first product nice one",
         price : 500,
-        model : 2022
+        model : 2022,
+        img : "first.jpeg"
     },
     {
         id : 1,
         name : "product-1",
         description : "first product nice one",
         price : 500,
-        model : 2022
+        model : 2022,
+        img : "sec.jpeg"
     }
 ]
---
+++ Redux/coustom-data/src/components/ownerData.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name : "Huzaifa Iqbal",
+        age : 20,
+        hobby : "learning, programming, gaming and outing",
+        sources : "self-taught",
+        experience : "10 months",
+        languages : "Jade(PUG), sass, html, htmlCanvas, css, bootstrap, javascript, reactJS, redux, styled-components, nodeJS, mongoose, mongoDB, mySQL, sequelize, C and python"
+    }
+]
