fb2b2292606d29388550f6c142eb7764a6281a14
huzidev <huzaifa.iqdev@gmail.com>
2022-07-18 11:57:25 +0500

    react router dom

--- Redux/coustom-data/src/components/App.js
+++ Redux/coustom-data/src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import OwnerInfo from './OwnerInfo';
+import Nav from './Nav';
+import Home from './Home'
+import OwnerInfo from './OwnerInfo'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
 
 export default function App() {
     return (
         <div>
-            <OwnerInfo />
+            <Router>
+                <Nav />
+                <Routes>
+                    <Route exact path='/'  element={<Home />}/>
+                    <Route exact path='/owner'  element={<OwnerInfo />}/>
+                </Routes>
+            </Router>
         </div>
     )
 }
---
+++ Redux/coustom-data/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <div>
+            <a href="/owner">Owner</a>
+        </div>
+    )
+}
