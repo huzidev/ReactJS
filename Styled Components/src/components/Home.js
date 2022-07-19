88ec82d1e493f1e155ac69cba48be70998d75d66
huzidev <huzaifa.iqdev@gmail.com>
2022-07-19 22:50:37 +0500

    fixing styled components button

---
+++ Styled Components/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './styled-components/Button';
+
+export default function Home() {
+    return (
+        <div>
+            <h3>
+                Home Page
+            </h3>
+            <Button>hello</Button>
+        </div>
+    )
+}
---
+++ Styled Components/src/components/styled-components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Button() {
+
+    const Button = styled.button`
+        background-color: aqua;
+        border: 1px solid black;
+        color: white;
+        border-radius: 3px;
+    `
+
+    return (
+        <>
+        </>
+    )
+}
