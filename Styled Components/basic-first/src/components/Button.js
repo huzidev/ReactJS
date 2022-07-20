a0306fc83b8ff65821d1e7730fa9dcf661902cba
huzidev <huzaifa.iqdev@gmail.com>
2022-07-20 13:16:28 +0500

    theme provider styled-components

--- Styled Components/basic-first/src/components/Button.js
+++ Styled Components/basic-first/src/components/Button.js
@@ -6,27 +6,45 @@ export default function Button() {
     // important to do because if we wanted to change a color at thousand of places so it'll take too much time for changing color at
     // every single places one by one hence it is good practice to just assign the color to the variable so when we wanted to change color
     // at thousand of places it'll be changed automatically since we've used that specific variables at all places
-    let color = "black";
-    let fontColor = "white";
-    let border = "blue";
+    const theme = {
+        color : {
+            border : "blue",
+            backgroundColor : "black",
+            fontColor : "white"
+        },
+        responsive : {
+            mobile : "800px",
+            tab : "1000px"
+        }
+    }
 
     const Button = styled.button`
         background-color: aqua;
-        border: 1px solid ${border};
+        border: 1px solid ${({theme}) => theme.color.border};
+        color: blue;
         font-size: 21px;
-        color: ${color};
         border-radius: 3px;
         cursor: pointer;
         padding: 5px 30px;
         transition: all 500ms ease-in-out;
         &:hover {
-            background-color: ${color};
-            color: ${fontColor};
+            background-color: ${({theme}) => theme.color.backgroundColor};
+            color: ${({theme}) => theme.color.fontColor};
+        }
+        @media only screen and (max-width:${({theme}) => theme.responsive.tab}) {
+            background-color: black;
+            color: white;
+
+        }
+        @media only screen and (max-width:${({theme}) => theme.responsive.mobile}) {
+            background-color: purple;
+            color: ${({theme}) => theme.color.fontColor};
         }
     `
 
     const Link = styled.a`
-        background-color: ${color};
+        background-color: ${({theme}) => theme.color.backgroundColor};
+        color: ${({theme}) => theme.color.fontColor};
         border: 1px solid black;
         font-size: 21px;
         color: white;
@@ -63,7 +81,8 @@ export default function Button() {
 
     return (
         <>
-            <ThemeProvider >
+            <ThemeProvider theme={theme}> 
+            {/* theme must have to be used as props */}
                 <Wrapper>
                     <Button>
                         test
