95cad42e0f63529cbf86456b616b3be463c838c8
huzidev <huzaifa.iqdev@gmail.com>
2022-07-20 09:58:53 +0500

    dsisplay flex with styled componets

--- Styled Components/basic-first/src/components/Button.js
+++ Styled Components/basic-first/src/components/Button.js
@@ -13,11 +13,33 @@ export default function Button() {
         padding: 5px 30px;
     `
 
+    const Link = styled.a`
+        background-color: black;
+        border: 1px solid black;
+        font-size: 21px;
+        color: white;
+        border-radius: 3px;
+        cursor: pointer;
+        padding: 5px 30px;
+        `
+    
+    const Wrapper = styled.section`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    `
+
     return (
         <>
-            <Button>
-                test
-            </Button>
+            <Wrapper>
+                <Button>
+                    test
+                </Button>
+                <Link>
+                    anchor tag
+                </Link>
+            </Wrapper>
         </>
     )
 }
