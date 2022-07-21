9225f1f529283655e4e98811f27721e31019f01d
huzidev <huzaifa.iqdev@gmail.com>
2022-07-21 22:00:35 +0500

    fixing headers

--- Ant-Design/basic-first/src/components/Header.js
+++ Ant-Design/basic-first/src/components/Header.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { MenuStyles, Wrapper } from './styled-components/Header.styled.js'
 
 export default function App() {
     const { Header } = Layout;
     return ( 
         <div>
             <Layout className="layout">
-                <Header>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['1']}
-                        items={new Array(2).fill(null).map((_, index) => {
-                            const key = index + 1;
-                            return {
-                                key,
-                                label: `nav ${key}`,
-                            };
-                        })}
-                    />
-                </Header>
+                <Wrapper>
+                    <MenuStyles>
+                        <Header>
+                            <Menu
+                                theme="dark"
+                                mode="horizontal"
+                                defaultSelectedKeys={['1']}
+                                items={new Array(2).fill(null).map((_, index) => {
+                                    const key = index + 1;
+                                    return {
+                                        key,
+                                        label: `nav ${key}`,
+                                    };
+                                })}
+                            />
+                        </Header>
+                    </MenuStyles>
+                </Wrapper>
             </Layout>
         </div>
     )
---
+++ Ant-Design/basic-first/src/components/styled-components/Header.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    background-color: aqua;
+`
+
+export const MenuStyles = styled.div`
+    width: 30%;
+    float: right;
+`
