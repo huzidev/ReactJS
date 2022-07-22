16cb5fc2faa7d5b0570c4ffd87cb3553bae56fc5
huzidev <huzaifa.iqdev@gmail.com>
2022-07-22 16:24:06 +0500

    zustand state management

--- Zustand/src/components/App.js
+++ Zustand/src/components/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import Header from './Header.js'
-import Hero from './Hero.js';
 
 export default function App() {
     return ( 
         <div>
-            <Header />
-            <Hero />
         </div>
     )
 }
