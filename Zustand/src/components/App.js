3889265755cae3f19da417985a8c7e85f600414f
huzidev <huzaifa.iqdev@gmail.com>
2022-07-22 16:38:47 +0500

    zustand fucntion works

--- Zustand/src/components/App.js
+++ Zustand/src/components/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import useStore from './Store';
 
 export default function App() {
+
+    const count = useStore((state) => state.count)
+
+    const inc = useStore((state) => state.inc)
+
+    const dec = useStore((state) => state.dec)
+
     return ( 
         <div>
+            <h1>
+                Initial Count : {count}
+            </h1>
+            <button onClick={inc}>
+                Increase Count
+            </button>
+            {
+                count === 0
+                    ? '' 
+                    : (
+                        <button onClick={dec}>
+                            Decrease Count
+                        </button>
+                    )
+            }
         </div>
     )
 }
