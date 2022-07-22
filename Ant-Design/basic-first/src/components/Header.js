07a35b222a7cba7cf6b98b86916d299a94249d61
huzidev <huzaifa.iqdev@gmail.com>
2022-07-22 15:20:11 +0500

    width fi

--- Ant-Design/basic-first/src/components/Header.js
+++ Ant-Design/basic-first/src/components/Header.js
@@ -1,37 +1,54 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { Wrapper, MenuStyle, LogoStyle } from './styled-components/Header.styled.js'
+import { FireOutlined } from '@ant-design/icons';
+import { Wrapper, MenuStyle, LogoStyle, LogoContainer } from './styled-components/Header.styled.js'
 
 export default function App() {
     const { Header } = Layout;
-
-    let array = ["test"]
-
+    
     return ( 
         <div>
             <Layout className="layout">
                 <Wrapper>
+                    <LogoStyle>
+                        <LogoContainer>
+                            <FireOutlined className='logo'/>
+                            <h3>
+                                logo
+                            </h3>
+                        </LogoContainer>
+                    </LogoStyle>
                     <MenuStyle>
                         <Header>
                             <Menu
                                 theme="dark"
                                 mode="horizontal"
-                                defaultSelectedKeys={['1']}
-                                items={new Array(5).fill(null).map((_, index) => {
-                                    const key = index + 1;
-                                    return {
-                                        key,
-                                        label: `${array} ${key}`,
-                                    };
-                                })}
-                            />
+                                defaultSelectedKeys={["home"]}
+                            >
+                                <Menu.Item key="home">
+                                    Home
+                                </Menu.Item>
+                                <Menu.Item key="about">
+                                    About
+                                </Menu.Item>
+                                <Menu.Item key="features">
+                                    Features
+                                </Menu.Item>
+                                <Menu.Item key="howitworks">
+                                    How it works
+                                </Menu.Item>
+                                <Menu.Item key="faq">
+                                    FAQ
+                                </Menu.Item>
+                                <Menu.Item key="pricing">
+                                    Pricing
+                                </Menu.Item>
+                                <Menu.Item key="contact">
+                                    Contact Us
+                                </Menu.Item>
+                            </Menu>
                         </Header>
                     </MenuStyle>
-                    <LogoStyle>
-                        <h3>
-                            logo
-                        </h3>
-                    </LogoStyle>
                 </Wrapper>
             </Layout>
         </div>
--- Ant-Design/basic-first/src/components/styled-components/Header.styled.js
+++ Ant-Design/basic-first/src/components/styled-components/Header.styled.js
@@ -1,27 +1,42 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    background-color: aqua;
     position: relative;
+    background-color: #001529;
 `
 
 export const MenuStyle = styled.div`
-    width: 50%;
+    width: 45%;
     float: right;
+    padding-left:50px;
 `
 
 export const LogoStyle = styled.div`
     position: absolute;
     background-color: #001529;
     height: 100%;
-    width: 50%;
+    width: 30%;
     float: left;
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    `
+
+export const LogoContainer = styled.div`
+    width: 30%;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     h3 {
-        margin-left: 70px;
+        font-size: 19px;
+        margin-top: 7px;
         color: white;
     }
+    .logo {
+        margin-right: 5px;
+        font-size: 23px;
+        color: white
+    } 
 `
 
